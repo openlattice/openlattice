0fcdc2dbe2a9ac41c0b07786a2841fab9ba90e06
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-07-23 18:24:50 -0700

    Revert "Refactor out background services"
    This reverts commit c362063789efc84be183944ecf0ab92799484ca6.

---
+++ indexer/src/main/java/com/openlattice/pods/ConductorSecurityPod.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ */
+
+package com.openlattice.pods;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConductorSecurityPod extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure( HttpSecurity http ) throws Exception {
+        http.authorizeRequests().antMatchers( "/**" ).permitAll().and().csrf().disable();
+    }
+
+}
