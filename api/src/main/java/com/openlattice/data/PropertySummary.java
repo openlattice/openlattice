02a16f28c047f1c31452446a0a91cd650c964fb9
Piper Keyes <pckeyes@stanford.edu>
2018-08-31 09:34:07 -0700

    fixed property summary class

--- api/src/main/java/com/openlattice/data/PropertySummary.java
+++ api/src/main/java/com/openlattice/data/PropertySummary.java
@@ -7,26 +7,18 @@ import com.openlattice.client.serialization.SerializationConstants;
 import java.util.UUID;
 
 public class PropertySummary {
-    private final UUID id;
     private final UUID entityTypeID;
     private final UUID entitySetID;
     private final int count;
 
     @JsonCreator
     public PropertySummary(
-            @JsonProperty (SerializationConstants.ID_FIELD ) UUID id,
             @JsonProperty( SerializationConstants.ENTITY_TYPE_ID ) UUID entityTypeID,
             @JsonProperty( SerializationConstants.ENTITY_SET_ID ) UUID entitySetID,
             @JsonProperty( SerializationConstants.COUNT) int count ) {
-        this.id = id;
         this.entityTypeID = entityTypeID;
         this.entitySetID = entitySetID;
         this.count = count;
-    }
-
-    @JsonProperty( SerializationConstants.ID_FIELD )
-    public UUID getPropertyTypeID() {
-        return id;
     }
 
     @JsonProperty( SerializationConstants.ENTITY_TYPE_ID )
