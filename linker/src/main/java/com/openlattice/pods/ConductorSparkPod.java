8cec0370b5501841ce4a6d719141ee2060ba4c7c
you-leee <julia@openlattice.com>
2018-09-10 11:34:56 -0700

    LATTICE-897 move es and pt creation, indexing before event fired

--- linker/src/main/java/com/openlattice/pods/ConductorSparkPod.java
+++ linker/src/main/java/com/openlattice/pods/ConductorSparkPod.java
@@ -20,39 +20,18 @@
 
 package com.openlattice.pods;
 
-import com.dataloom.mappers.ObjectMappers;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.eventbus.EventBus;
-import com.google.common.util.concurrent.ListeningExecutorService;
 import com.hazelcast.core.HazelcastInstance;
-import com.kryptnostic.rhizome.core.Cutting;
-import com.openlattice.authorization.AbstractSecurableObjectResolveTypeService;
-import com.openlattice.authorization.AuthorizationManager;
-import com.openlattice.authorization.AuthorizationQueryService;
-import com.openlattice.authorization.HazelcastAbstractSecurableObjectResolveTypeService;
-import com.openlattice.authorization.HazelcastAclKeyReservationService;
-import com.openlattice.authorization.HazelcastAuthorizationService;
 import com.openlattice.conductor.rpc.ConductorConfiguration;
 import com.openlattice.conductor.rpc.ConductorElasticsearchApi;
-import com.openlattice.data.DatasourceManager;
 import com.openlattice.data.storage.PostgresEntityDataQueryService;
-import com.openlattice.datastore.services.EdmManager;
-import com.openlattice.datastore.services.EdmService;
-import com.openlattice.edm.PostgresEdmManager;
-import com.openlattice.edm.properties.PostgresTypeManager;
-import com.openlattice.edm.schemas.SchemaQueryService;
-import com.openlattice.edm.schemas.manager.HazelcastSchemaManager;
-import com.openlattice.edm.schemas.postgres.PostgresSchemaQueryService;
-import com.openlattice.hazelcast.HazelcastQueue;
 import com.openlattice.indexing.BackgroundIndexingService;
 import com.openlattice.kindling.search.ConductorElasticsearchImpl;
-import com.openlattice.linking.HazelcastBlockingService;
-import com.openlattice.mail.config.MailServiceRequirements;
 import com.zaxxer.hikari.HikariDataSource;
-import java.io.IOException;
-import javax.inject.Inject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 @Configuration
 public class ConductorSparkPod {
@@ -64,90 +43,8 @@ public class ConductorSparkPod {
     private ConductorConfiguration conductorConfiguration;
 
     @Inject
-    private EventBus eventBus;
-
-    @Inject
-    private Cutting cutting;
-
-    @Inject
     private HikariDataSource hikariDataSource;
 
-    @Inject
-    private ListeningExecutorService executorService;
-
-    @Bean
-    public ObjectMapper defaultObjectMapper() {
-        return ObjectMappers.getJsonMapper();
-    }
-
-    @Bean
-    public AuthorizationQueryService authorizationQueryService() {
-        return new AuthorizationQueryService( hikariDataSource, hazelcastInstance );
-    }
-
-    @Bean
-    public AuthorizationManager authorizationManager() {
-        return new HazelcastAuthorizationService( hazelcastInstance, authorizationQueryService(), eventBus );
-    }
-
-    @Bean
-    public AbstractSecurableObjectResolveTypeService securableObjectTypes() {
-        return new HazelcastAbstractSecurableObjectResolveTypeService( hazelcastInstance );
-    }
-
-    @Bean
-    public SchemaQueryService schemaQueryService() {
-        return new PostgresSchemaQueryService( hikariDataSource );
-    }
-
-    @Bean
-    public PostgresEdmManager edmManager() {
-        return new PostgresEdmManager( hikariDataSource );
-    }
-
-    @Bean
-    public HazelcastSchemaManager schemaManager() {
-        return new HazelcastSchemaManager( hazelcastInstance, schemaQueryService() );
-    }
-
-    @Bean
-    public PostgresTypeManager entityTypeManager() {
-        return new PostgresTypeManager( hikariDataSource );
-    }
-
-    @Bean
-
-    public HazelcastAclKeyReservationService aclKeyReservationService() {
-        return new HazelcastAclKeyReservationService( hazelcastInstance );
-    }
-
-    @Bean
-    public MailServiceRequirements mailServiceRequirements() {
-        return () -> hazelcastInstance.getQueue( HazelcastQueue.EMAIL_SPOOL.name() );
-    }
-
-    @Bean
-    public DatasourceManager datasourceManager() {
-        return new DatasourceManager( hikariDataSource, hazelcastInstance );
-    }
-
-    @Bean
-    public PostgresEntityDataQueryService dataQueryService() {
-        return new PostgresEntityDataQueryService( hikariDataSource );
-    }
-
-    @Bean
-    public EdmManager dataModelService() {
-        return new EdmService(
-                hikariDataSource,
-                hazelcastInstance,
-                aclKeyReservationService(),
-                authorizationManager(),
-                edmManager(),
-                entityTypeManager(),
-                schemaManager(),
-                datasourceManager() );
-    }
 
     @Bean
     public ConductorElasticsearchApi elasticsearchApi() throws IOException {
@@ -155,8 +52,8 @@ public class ConductorSparkPod {
     }
 
     @Bean
-    public HazelcastBlockingService blockingService() {
-        return new HazelcastBlockingService( hazelcastInstance );
+    public PostgresEntityDataQueryService dataQueryService() {
+        return new PostgresEntityDataQueryService( hikariDataSource );
     }
 
     @Bean
