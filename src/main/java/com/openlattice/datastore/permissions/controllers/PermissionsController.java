b66f03c3eb7b864cf3a251097cd48d037abf81a1
Piper Keyes <pckeyes@stanford.edu>
2018-09-14 15:44:29 -0700

    changed implementation of getAclExplanation

--- src/main/java/com/openlattice/datastore/permissions/controllers/PermissionsController.java
+++ src/main/java/com/openlattice/datastore/permissions/controllers/PermissionsController.java
@@ -20,31 +20,21 @@
 
 package com.openlattice.datastore.permissions.controllers;
 
+import com.dataloom.streams.StreamUtil;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.SetMultimap;
 import com.google.common.eventbus.EventBus;
-import com.openlattice.authorization.Ace;
-import com.openlattice.authorization.AceExplanation;
-import com.openlattice.authorization.Acl;
-import com.openlattice.authorization.AclData;
-import com.openlattice.authorization.AclExplanation;
-import com.openlattice.authorization.AclKey;
-import com.openlattice.authorization.AuthorizationManager;
-import com.openlattice.authorization.AuthorizingComponent;
-import com.openlattice.authorization.ForbiddenException;
-import com.openlattice.authorization.Permission;
-import com.openlattice.authorization.PermissionsApi;
-import com.openlattice.authorization.Principal;
-import com.openlattice.authorization.PrincipalType;
+import com.openlattice.authorization.*;
 import com.openlattice.datastore.exceptions.BadRequestException;
 import com.openlattice.organizations.roles.SecurePrincipalsManager;
-import java.util.Collection;
-import java.util.EnumSet;
+
+import java.time.OffsetDateTime;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -85,7 +75,8 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
                             ace -> authorizations.addPermission(
                                     aclKeys,
                                     ace.getPrincipal(),
-                                    ace.getPermissions() ) );
+                                    ace.getPermissions(),
+                                    ace.getExpirationDate() ) );
                     break;
                 case REMOVE:
                     acl.getAces().forEach(
@@ -99,7 +90,8 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
                             ace -> authorizations.setPermission(
                                     aclKeys,
                                     ace.getPrincipal(),
-                                    ace.getPermissions() ) );
+                                    ace.getPermissions(),
+                                    ace.getExpirationDate() ) );
                     break;
                 default:
                     logger.error( "Invalid action {} specified for request.", req.getAction() );
@@ -125,6 +117,7 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
         }
     }
 
+    /*
     @Override
     @RequestMapping(
             path = { EXPLAIN },
@@ -132,11 +125,19 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE )
     public AclExplanation getAclExplanation( @RequestBody AclKey aclKey ) {
+        //needs to return a map of principles to the path that grants permission with expiration
         if ( isAuthorized( Permission.OWNER ).test( aclKey ) ) {
-            SetMultimap<Principal, Ace> resultMap = HashMultimap.create();
+            SetMultimap<Principal, Ace> resultMap = HashMultimap
+                    .create(); //will hold all aces that contain the key principal
 
-            Iterable<Ace> aces = authorizations.getAllSecurableObjectPermissions( aclKey ).getAces();
+            Iterable<Ace> aces = authorizations.getAllSecurableObjectPermissions( aclKey )
+                    .getAces(); //gets aces from returned acl
+            Map<PrincipalType, List<Ace>> acesByType = StreamUtil.stream( aces )
+                    .collect( Collectors.groupingBy( ace -> ace.getPrincipal().getType() ) );
+            //for principals that aren't users, sPM.getallPrincipalsbyPrincipal
+            Map<Principal, Set<Principal>>
             for ( Ace ace : aces ) {
+                //build all paths for how a person has access
                 Principal principal = ace.getPrincipal();
                 resultMap.put( principal, ace );
                 if ( principal.getType() == PrincipalType.ROLE ) {
@@ -144,7 +145,7 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
                     Iterable<Principal> users = securePrincipalsManager
                             .getAllUsersWithPrincipal( securePrincipalsManager.lookup( principal ) );
 
-                    for ( Principal user : users ) {
+                    for ( Principal user : users ) { //can add a new ace to a principal that is already in the multimap
                         resultMap.put( user, ace );
                     }
                 }
@@ -152,23 +153,81 @@ public class PermissionsController implements PermissionsApi, AuthorizingCompone
 
             // compute total permission of each user
             Iterable<AceExplanation> explanation = Iterables.transform( resultMap.asMap().entrySet(),
-                    this::computeAceExplanation );
+                    this::computeAceExplanation ); // passes entry in as key to collection of all aces
             return new AclExplanation( aclKey, explanation::iterator );
         } else {
             throw new ForbiddenException( "Only owner of a securable object can access other users' access rights." );
         }
     }
 
-    /**
-     * Compute the total permission of a user has from his aces, thus computing the Ace explanation
-     */
+
+    // Compute the total permission of a user has from his aces, thus computing the Ace explanation
+
     private AceExplanation computeAceExplanation( Entry<Principal, Collection<Ace>> entry ) {
         Set<Permission> totalPermissions = entry.getValue().stream().flatMap( ace -> ace.getPermissions().stream() )
                 .collect( Collectors.toCollection( () -> EnumSet.noneOf( Permission.class ) ) );
+        //OffsetDateTime expirationDate = entry.getValue().stream().flatMap(ace -> ace.getExpirationDate()).collect(  );
         Set<Ace> aces = entry.getValue().stream().collect( Collectors.toSet() );
         Ace totalAce = new Ace( entry.getKey(), totalPermissions );
-        return new AceExplanation( totalAce, aces );
+        return new AceExplanation( totalAce, aces ); //list of securablePrincipals with expiration date
     }
+    */
+
+    @Override
+    @RequestMapping(
+            path = { EXPLAIN },
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE )
+    public Map<Principal, List<List<Principal>>> getAclExplanation( @RequestBody AclKey aclKey ) {
+        //needs to return a map of principles to the path that grants permission with expiration
+        ensureOwnerAccess( aclKey );
+        SetMultimap<Principal, Ace> resultMap = HashMultimap
+                .create(); //will hold all aces that contain the key principal
+
+        Iterable<Ace> aces = authorizations.getAllSecurableObjectPermissions( aclKey )
+                .getAces(); //gets aces from returned acl
+        Map<PrincipalType, List<Ace>> acesByType = StreamUtil.stream( aces )
+                .collect( Collectors.groupingBy( ace -> ace.getPrincipal().getType() ) );
+        //for principals that aren't users, sPM.getallPrincipalsbyPrincipal gets all of the parents of those principals
+        Map<Principal, List<List<Principal>>> principalToPrincipalPaths = new HashMap<>();
+        //for all principals with direct access
+        for ( Ace ace : aces ) {
+            //build all paths for how a person has access
+            Principal principal = ace.getPrincipal();
+            //OffsetDateTime expirationDate = ace.getExpirationDate();
+            List<List<Principal>> paths = new ArrayList<>();
+
+            principalToPrincipalPaths.put( principal, paths );
+
+            //get parent layer
+            Collection<SecurablePrincipal> parentLayer = securePrincipalsManager
+                    .getParentPrincipalsOfPrincipal( securePrincipalsManager.lookup( principal ) );
+            //if parent layer isn't empty, start recursively getting paths
+            if ( !parentLayer.isEmpty() ) {
+                for ( SecurablePrincipal parent : parentLayer ) {
+                    paths = securePrincipalsManager
+                            .recursivelyGetPrincipalsPath( parent.getPrincipal(), principalToPrincipalPaths );
+                    paths.forEach( path -> path.add(0, principal) );
+                    principalToPrincipalPaths.put(principal, paths);
+                }
+            }
+        }
+        return principalToPrincipalPaths;
+
+    }
+
+    /*
+     //Compute the total permission of a user has from his aces, thus computing the Ace explanation
+    private AceExplanation computeAceExplanation( Entry<Principal, Collection<Ace>> entry ) {
+        Set<Permission> totalPermissions = entry.getValue().stream().flatMap( ace -> ace.getPermissions().stream() )
+                .collect( Collectors.toCollection( () -> EnumSet.noneOf( Permission.class ) ) );
+        //OffsetDateTime expirationDate = entry.getValue().stream().flatMap(ace -> ace.getExpirationDate()).collect(  );
+        Set<Ace> aces = entry.getValue().stream().collect( Collectors.toSet() );
+        Ace totalAce = new Ace( entry.getKey(), totalPermissions );
+        return new AceExplanation( totalAce, aces ); //list of securablePrincipals with expiration date
+    }
+    */
 
     @Override
     public AuthorizationManager getAuthorizationManager() {
