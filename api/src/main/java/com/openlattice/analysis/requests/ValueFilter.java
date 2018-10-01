9b7354d30f7e0b51a6483e331022925b9907a1d4
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-10-01 13:32:59 -0700

    Fix joining

--- api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
+++ api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
@@ -6,14 +6,13 @@ import java.util.stream.Collectors;
 public class ValueFilter<T extends Comparable<T>> implements Filter<T> {
     private Set<T> values;
 
-    public ValueFilter(Set<T> values) {
+    public ValueFilter( Set<T> values ) {
         this.values = values;
     }
 
     @Override
-    public String asSql(String field) {
+    public String asSql( String field ) {
         return field +
-                " IN (" + String.join(", ", values.stream().map(
-                        val -> val.toString()).collect(Collectors.toList())) + ")";
+                " IN (" + values.stream().map( Object::toString ).collect( Collectors.joining( "," ) ) + ")";
     }
 }
