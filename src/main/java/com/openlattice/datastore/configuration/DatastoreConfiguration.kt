e20731184cc842de8b57240dcbf73065b5c014f7
Piper Keyes <pkeyes12@gmail.com>
2018-11-02 12:18:49 -0700

    added time to live property

--- src/main/java/com/openlattice/datastore/configuration/DatastoreConfiguration.kt
+++ src/main/java/com/openlattice/datastore/configuration/DatastoreConfiguration.kt
@@ -1,5 +1,6 @@
 package com.openlattice.datastore.configuration
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.kryptnostic.rhizome.configuration.Configuration;
 import com.kryptnostic.rhizome.configuration.ConfigurationKey
@@ -9,11 +10,16 @@ import com.openlattice.client.serialization.SerializationConstants
 
 @ReloadableConfiguration(uri = "datastore.yaml")
 data class DatastoreConfiguration(
-        @JsonProperty(SerializationConstants.BUCKET_NAME) val bucketName: String ): Configuration {
+        @JsonProperty(SerializationConstants.BUCKET_NAME) val bucketName: String,
+        @JsonProperty(SerializationConstants.TIME_TO_LIVE) val urlTTL: Long): Configuration {
     val serialVersionUID: Long = -3847142110887587615L //this is copied from ConstructorConfiguration.java, need to figure out how to generate one
     val key: SimpleConfigurationKey = SimpleConfigurationKey("datastore.yaml")
 
+    @JsonIgnore
     override fun getKey(): ConfigurationKey {
         return key
     }
+
+    @JsonIgnore
+    fun key(): ConfigurationKey { return key }
 }
