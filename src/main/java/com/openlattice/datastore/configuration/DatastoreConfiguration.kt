b364f680b0ee6c630887dd827f3173ab9a8d879a
Piper Keyes <pkeyes12@gmail.com>
2018-11-14 10:22:51 -0800

    added companion object to DatastoreConfiguration

--- src/main/java/com/openlattice/datastore/configuration/DatastoreConfiguration.kt
+++ src/main/java/com/openlattice/datastore/configuration/DatastoreConfiguration.kt
@@ -1,12 +1,10 @@
 package com.openlattice.datastore.configuration
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.kryptnostic.rhizome.configuration.Configuration;
 import com.kryptnostic.rhizome.configuration.ConfigurationKey
 import com.kryptnostic.rhizome.configuration.SimpleConfigurationKey
 import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration
-import com.openlattice.client.serialization.SerializationConstants
 
 @ReloadableConfiguration(uri = "datastore.yaml")
 data class DatastoreConfiguration(
@@ -15,13 +13,14 @@ data class DatastoreConfiguration(
         @JsonProperty("timeToLive") val timeToLive: Long,
         @JsonProperty("accessKeyId") val accessKeyId: String,
         @JsonProperty("secretAccessKey") val secretAccessKey: String): Configuration {
-    val key = SimpleConfigurationKey("datastore.yaml")
 
-    @JsonIgnore
+    companion object {
+        @JvmStatic
+        @get:JvmName("key")
+        val key = SimpleConfigurationKey("datastore.yaml")
+    }
+
     override fun getKey(): ConfigurationKey {
         return key
     }
-
-    @JsonIgnore
-    fun key(): ConfigurationKey { return key }
 }
