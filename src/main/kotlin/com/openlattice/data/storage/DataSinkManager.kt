e3507c42a37678d7aca5c8e735eca85fd1648f13
Piper Keyes <pkeyes12@gmail.com>
2018-11-27 19:12:43 -0800

    renamed class

--- src/main/kotlin/com/openlattice/data/storage/DataSinkManager.kt
+++ src/main/kotlin/com/openlattice/data/storage/DataSinkManager.kt
@@ -1,11 +1,10 @@
 package com.openlattice.data.storage
 
 import com.openlattice.data.IntegrationResults
-import com.openlattice.data.integration.DataSinkObject
-import com.openlattice.data.integration.EntityIdsAndData
+import com.openlattice.data.integration.EntityData
 import com.openlattice.edm.type.PropertyType
 import java.util.*
 
 interface DataSinkManager {
-    fun integrateEntities(entities: Set<EntityIdsAndData>, authorizedPropertiesByEntitySetId: Map<UUID, Map<UUID, PropertyType>>): IntegrationResults?
+    fun integrateEntities(entities: Set<EntityData>, authorizedPropertiesByEntitySetId: Map<UUID, Map<UUID, PropertyType>>): IntegrationResults?
 }
