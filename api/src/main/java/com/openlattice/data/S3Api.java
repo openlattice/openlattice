cf5358a7fcd4670cb593989298e3b24c7f310d46
Piper Keyes <pkeyes12@gmail.com>
2018-12-03 15:52:26 -0800

    created api to write to s3 from shuttle

--- api/src/main/java/com/openlattice/data/S3Api.java
+++ api/src/main/java/com/openlattice/data/S3Api.java
@@ -1,4 +1,20 @@
 package com.openlattice.data;
 
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.PUT;
+import retrofit2.http.Url;
+
 public interface S3Api {
+
+    String SERVICE    = "/datastore";
+    String CONTROLLER = "/integration";
+    String BASE       = SERVICE + CONTROLLER;
+
+    @PUT
+    Call<Void> writeToS3(
+            @Url String url,
+            @Body RequestBody data
+    );
 }
