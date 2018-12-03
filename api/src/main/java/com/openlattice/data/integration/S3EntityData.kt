e7df5e8b8d8f0c4e069521a75aaf56b581940965
Piper Keyes <pkeyes12@gmail.com>
2018-12-03 15:54:05 -0800

    created class for writing to s3

--- api/src/main/java/com/openlattice/data/integration/S3EntityData.kt
+++ api/src/main/java/com/openlattice/data/integration/S3EntityData.kt
@@ -1,2 +1,12 @@
 package com.openlattice.data.integration
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.*
+
+data class S3EntityData(
+        @JsonProperty val entitySetId: UUID,
+        @JsonProperty val entityKeyId: UUID,
+        @JsonProperty val propertyTypeId: UUID,
+        @JsonProperty val propertyHash: String,
+        @JsonProperty val properties: ByteArray
+)
