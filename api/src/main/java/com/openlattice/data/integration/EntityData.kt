e77e86ea6e40b26455904e3df50fd918c9beb538
Piper Keyes <pkeyes12@gmail.com>
2018-12-04 10:59:18 -0800

    corrected documentation

--- api/src/main/java/com/openlattice/data/integration/EntityData.kt
+++ api/src/main/java/com/openlattice/data/integration/EntityData.kt
@@ -3,7 +3,7 @@ package com.openlattice.data.integration
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-/* Contains entity set id, entity id, and entity key id of a given entity
+/* Contains entity set id and entity key id of a given entity
 * and a map of property type id to properties that belong to that entity
 */
 
