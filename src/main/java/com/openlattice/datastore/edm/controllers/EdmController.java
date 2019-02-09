e5a90134b1ebe39df887f3eb344bb906b0b5ca83
Katherine Bernstein <katherine@openlattice.com>
2019-02-08 16:07:11 -0800

    cleanup

--- src/main/java/com/openlattice/datastore/edm/controllers/EdmController.java
+++ src/main/java/com/openlattice/datastore/edm/controllers/EdmController.java
@@ -79,7 +79,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
