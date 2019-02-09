70710e46b0d42f5b51fabf91a2c8c31f48e63ad6
Katherine Bernstein <katherine@openlattice.com>
2019-02-08 16:05:50 -0800

    bulk load entity set property metadata for multiple entity sets

--- src/main/java/com/openlattice/datastore/edm/controllers/EdmController.java
+++ src/main/java/com/openlattice/datastore/edm/controllers/EdmController.java
@@ -66,6 +66,7 @@ import com.openlattice.edm.type.PropertyType;
 import com.openlattice.exceptions.ApiExceptions;
 import com.openlattice.exceptions.ErrorsDTO;
 import com.openlattice.web.mediatypes.CustomMediaType;
+
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -75,8 +76,10 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -1025,7 +1028,7 @@ public class EdmController implements EdmApi, AuthorizingComponent {
                     }
                 } );
 
-        return Maps.toMap( entitySetIds, modelService::getAllEntitySetPropertyMetadata );
+        return modelService.getAllEntitySetPropertyMetadataForIds( entitySetIds );
     }
 
     @Override
