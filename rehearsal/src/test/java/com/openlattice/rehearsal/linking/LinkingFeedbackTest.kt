0ef8fc515e210ad88372041878a5d19ebb919320
you-leee <julia@openlattice.com>
2019-02-14 10:08:15 -0800

    LATTICE-1666 improve automatic entityset creation, deletion in tests

--- rehearsal/src/test/java/com/openlattice/rehearsal/linking/LinkingFeedbackTest.kt
+++ rehearsal/src/test/java/com/openlattice/rehearsal/linking/LinkingFeedbackTest.kt
@@ -1,6 +1,52 @@
 package com.openlattice.rehearsal.linking
 
+import com.openlattice.edm.type.EntityType
 import com.openlattice.rehearsal.SetupTestData
+import com.openlattice.rehearsal.edm.PERSON_NAME
+import com.openlattice.rehearsal.edm.PERSON_NAMESPACE
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import java.lang.reflect.UndeclaredThrowableException
 
-class LinkingFeedbackTest: SetupTestData() {
+class LinkingFeedbackTest : SetupTestData() {
+    private val numberOfEntries = 10
+
+    companion object {
+        private val importedEntitySets = mapOf(
+                "SocratesTestA" to Pair("socratesA.yaml", "test_linked_person_1.csv"),
+                "SocratesTestB" to Pair("socratesB.yaml", "test_linked_person_2.csv"),
+                "SocratesTestC" to Pair("socratesC.yaml", "test_linked_person_3.csv"),
+                "SocratesTestD" to Pair("socratesD.yaml", "test_not_linked_person_1.csv"))
+
+        lateinit var personEt: EntityType
+
+        @JvmStatic
+        @BeforeClass
+        fun init() {
+            importedEntitySets.forEach {
+                importDataSet(it.value.first, it.value.second)
+            }
+
+            Thread.sleep(10000L)
+            while (!checkLinkingFinished(importedEntitySets.keys)) {
+                Thread.sleep(5000L)
+            }
+
+            loginAs("admin")
+            personEt = edmApi.getEntityType(edmApi.getEntityTypeId(PERSON_NAMESPACE, PERSON_NAME))
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun tearDown() {
+            importedEntitySets.keys.forEach {
+                try {
+                    edmApi.deleteEntitySet(edmApi.getEntitySetId(it))
+                } catch (e: UndeclaredThrowableException) {
+                }
+            }
+        }
+    }
+
+
 }
