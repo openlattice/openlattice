cd22ea007d09f2fcae1fdc85c1a3ec499bfe67be
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-02-17 18:43:05 -0800

    Multiple fixes for integrations and s3 storage
    - Clean up how objects are passed from JsonDeserializer to data query service
    - Consolidate repeated code in AwsSinkService
    - Fix bug in AwsDataSink service that was using entity set id as entity key id
    - Improve performance for updating versions during updates as well as use SELECT FOR SHARE to avoid deadlocks.

--- src/main/kotlin/com/openlattice/data/storage/AwsDataSinkService.kt
+++ src/main/kotlin/com/openlattice/data/storage/AwsDataSinkService.kt
@@ -13,90 +13,45 @@ import org.springframework.stereotype.Service
 import java.net.URL
 import java.sql.PreparedStatement
 import java.util.*
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 
 private val logger = LoggerFactory.getLogger(AwsDataSinkService::class.java)
 
-@Service
-class AwsDataSinkService {
-    @Inject
-    lateinit var byteBlobDataManager: ByteBlobDataManager
-    @Inject
-    lateinit var hds: HikariDataSource
 
-    fun generatePresignedUrls(entities: List<S3EntityData>, authorizedPropertyTypes: Map<UUID, Map<UUID, PropertyType>>): List<String> {
-        val postgresData = mutableMapOf<S3EntityData, String>()
+class AwsDataSinkService(
+        private val byteBlobDataManager: ByteBlobDataManager,
+        private val hds: HikariDataSource
+) {
+    private val dqs = PostgresEntityDataQueryService(hds, byteBlobDataManager)
+
+    fun generatePresignedUrls(
+            entities: List<S3EntityData>, authorizedPropertyTypes: Map<UUID, Map<UUID, PropertyType>>
+    ): List<String> {
+        val data = mutableMapOf<UUID, MutableMap<UUID, MutableMap<UUID, MutableSet<Any>>>>()
         val urls = Lists.newArrayList<String>()
         val expirationTime = Date()
         val timeToLive = expirationTime.time + 6000000
         expirationTime.time = timeToLive
         entities.forEach {
-            val key = it.entitySetId.toString() + "/" + it.entityKeyId.toString() + "/" + it.propertyTypeId.toString() + "/" + it.propertyHash
-            val url = byteBlobDataManager.getPresignedUrl(key, expirationTime, HttpMethod.PUT, Optional.empty())
-            postgresData[it] = key
-            urls.add(url.toString())
+            val key = "${it.entitySetId}/${it.entityKeyId}/${it.propertyTypeId}/${it.propertyHash}"
+            val url = byteBlobDataManager
+                    .getPresignedUrl(key, expirationTime, HttpMethod.PUT, Optional.empty())
+                    .toString()
+
+            data
+                    .getOrPut(it.entitySetId) { mutableMapOf() }
+                    .getOrPut(it.entityKeyId) { mutableMapOf() }
+                    .getOrPut(it.propertyTypeId) { mutableSetOf() }
+                    .add(key)
+
+            urls.add(url)
         }
         //write s3Keys to postgres
-        postgresData.forEach {
-            writeToPostgres(it, authorizedPropertyTypes[it.key.entitySetId]!!)
+        data.forEach { entitySetId, data ->
+            dqs.upsertEntities(entitySetId, data, authorizedPropertyTypes.getValue(entitySetId), true)
         }
+
         return urls
     }
-
-    private fun writeToPostgres(data: Map.Entry<S3EntityData, String>, authorizedPropertyTypes: Map<UUID, PropertyType>) {
-        val connection = hds.connection
-        connection.use {
-            val version = System.currentTimeMillis()
-            val entitySetId = data.key.entitySetId
-            val entitySetPreparedStatement = connection.prepareStatement(upsertEntity(entitySetId, version))
-            val datatypes = authorizedPropertyTypes.map {
-                it.key to it.value.datatype
-            }.toMap()
-            val preparedStatements = authorizedPropertyTypes
-                    .map {
-                        it.key to connection.prepareStatement(
-                                upsertPropertyValues(
-                                        entitySetId, it.key, it.value.type.fullQualifiedNameAsString, version
-                                )
-                        )
-                    }
-                    .toMap()
-
-            entitySetPreparedStatement.setObject(1, entitySetId)
-            entitySetPreparedStatement.addBatch()
-
-            val entityKeyId = data.key.entityKeyId
-            val propertyTypeId = data.key.propertyTypeId
-            val propertyHash = data.key.propertyHash
-            val s3Key = "$entitySetId/$entityKeyId/$propertyTypeId/$propertyHash"
-
-            if (s3Key == null) {
-                logger.error(
-                        "Encountered null property value of type {} for entity set {} with entity key id {}",
-                        propertyTypeId, entitySetId, entityKeyId
-                )
-            } else {
-                val hashAsBytes = ByteArray(propertyHash.length / 2) { propertyHash.substring(it * 2, it * 2 + 2).toInt(16).toByte() }
-                val ps = preparedStatements[propertyTypeId]
-                ps?.setObject(1, entityKeyId)
-                ps?.setBytes(2, hashAsBytes)
-                ps?.setObject(3, s3Key)
-                ps?.addBatch()
-                if (ps == null) {
-                    logger.warn(
-                            "Skipping unauthorized property in entity $entityKeyId from entity set $entitySetId"
-                    )
-                }
-            }
-
-            val updatedPropertyCounts = preparedStatements.values.map { it.executeBatch() }.sumBy { it.sum() }
-            val updatedEntityCount = entitySetPreparedStatement.executeBatch().sum()
-            preparedStatements.values.forEach(PreparedStatement::close)
-            entitySetPreparedStatement.close()
-
-            logger.debug("Updated $updatedEntityCount entities and $updatedPropertyCounts properties")
-        }
-
-    }
-
 }
