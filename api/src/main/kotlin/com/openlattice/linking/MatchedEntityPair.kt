64f15e785932cbeecd03385d498bbb1568aa2812
you-leee <julia@openlattice.com>
2019-02-18 10:37:13 -0800

    LATTICE-1666 add licence, add hashcode for matched entities

--- api/src/main/kotlin/com/openlattice/linking/MatchedEntityPair.kt
+++ api/src/main/kotlin/com/openlattice/linking/MatchedEntityPair.kt
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2019. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
 package com.openlattice.linking
 
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -11,5 +32,9 @@ data class MatchedEntityPair(
         if(other == null) return false
         if(other !is MatchedEntityPair) return false
         return entityPair == other.entityPair
+    }
+
+    override fun hashCode(): Int {
+        return 37 * entityPair.hashCode() + match.hashCode()
     }
 }
