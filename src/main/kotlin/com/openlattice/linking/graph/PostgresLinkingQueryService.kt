e4c532ec19482c3aaa716b15ec495f8708eaf1f2
you-leee <julia@openlattice.com>
2019-02-18 11:15:09 -0800

    LATTICE-1666 bump copyright year

--- src/main/kotlin/com/openlattice/linking/graph/PostgresLinkingQueryService.kt
+++ src/main/kotlin/com/openlattice/linking/graph/PostgresLinkingQueryService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018. OpenLattice, Inc.
+ * Copyright (C) 2019. OpenLattice, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
