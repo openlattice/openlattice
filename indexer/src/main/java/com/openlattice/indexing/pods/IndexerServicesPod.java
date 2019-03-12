39d33e0a6816ff22aa98fea73fa70dd30848aaff
you-leee <julia@openlattice.com>
2019-03-12 13:20:39 -0700

    LATTICE-1877 remove userdirectoryservice from HazelcastOrganizationService

--- indexer/src/main/java/com/openlattice/indexing/pods/IndexerServicesPod.java
+++ indexer/src/main/java/com/openlattice/indexing/pods/IndexerServicesPod.java
@@ -176,7 +176,6 @@ public class IndexerServicesPod {
                 hazelcastInstance,
                 aclKeyReservationService(),
                 authorizationManager(),
-                userDirectoryService(),
                 principalService(),
                 assembler() );
     }
