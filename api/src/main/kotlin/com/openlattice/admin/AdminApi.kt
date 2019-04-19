2edb10c2bc6fcc7dd37ff2b0385bf6b7efb987fd
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-04-18 18:50:28 -0700

    woops

--- api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
+++ api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
@@ -26,13 +26,13 @@ interface AdminApi {
      * Retrieve all the events record for a particular entity set in a given time window.
      * @param auditEntitySet The
      */
-    @GET(com.openlattice.admin.indexing.BASE + RELOAD_CACHE)
+    @GET(BASE + RELOAD_CACHE)
     fun reloadCache()
 
-    @GET(com.openlattice.admin.indexing.BASE + RELOAD_CACHE + com.openlattice.admin.indexing.NAME_PATH)
-    fun reloadCache(@Path(com.openlattice.admin.indexing.NAME) name: String)
+    @GET(BASE + RELOAD_CACHE + NAME_PATH)
+    fun reloadCache(@Path(NAME) name: String)
 
-    @GET(com.openlattice.admin.indexing.BASE + com.openlattice.admin.indexing.PRINCIPALS + com.openlattice.admin.indexing.ID_PATH)
-    fun getUserPrincipals( @Path(com.openlattice.admin.indexing.ID) principalId :String ): Set<Principal>
+    @GET(BASE + PRINCIPALS + ID_PATH)
+    fun getUserPrincipals(@Path(ID) principalId: String): Set<Principal>
 
 }
