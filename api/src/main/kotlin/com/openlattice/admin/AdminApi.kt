abc0016f1f964fdee2f4e265c42c33d8c247d967
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-04-18 18:38:13 -0700

    Implement indexing admin api

--- api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
+++ api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
@@ -8,16 +8,16 @@ import retrofit2.http.Path
 // @formatter:off
 const val SERVICE = "/datastore"
 const val CONTROLLER = "/admin"
-const val BASE = SERVICE + CONTROLLER
+const val BASE = com.openlattice.admin.indexing.SERVICE + com.openlattice.admin.indexing.CONTROLLER
 // @formatter:on
 
 const val RELOAD_CACHE = "/reload/cache"
 const val PRINCIPALS = "/principals"
 
 const val ID = "id"
-const val ID_PATH = "/{$ID}"
+const val ID_PATH = "/{${com.openlattice.admin.indexing.ID}}"
 const val NAME = "name"
-const val NAME_PATH = "/{$NAME}"
+const val NAME_PATH = "/{${com.openlattice.admin.indexing.NAME}}"
 
 interface AdminApi {
 
@@ -26,13 +26,13 @@ interface AdminApi {
      * Retrieve all the events record for a particular entity set in a given time window.
      * @param auditEntitySet The
      */
-    @GET(BASE + RELOAD_CACHE)
+    @GET(com.openlattice.admin.indexing.BASE + RELOAD_CACHE)
     fun reloadCache()
 
-    @GET(BASE + RELOAD_CACHE + NAME_PATH)
-    fun reloadCache(@Path(NAME) name: String)
+    @GET(com.openlattice.admin.indexing.BASE + RELOAD_CACHE + com.openlattice.admin.indexing.NAME_PATH)
+    fun reloadCache(@Path(com.openlattice.admin.indexing.NAME) name: String)
 
-    @GET( BASE + PRINCIPALS + ID_PATH )
-    fun getUserPrincipals( @Path(ID) principalId :String ): Set<Principal>
+    @GET(com.openlattice.admin.indexing.BASE + com.openlattice.admin.indexing.PRINCIPALS + com.openlattice.admin.indexing.ID_PATH)
+    fun getUserPrincipals( @Path(com.openlattice.admin.indexing.ID) principalId :String ): Set<Principal>
 
 }
