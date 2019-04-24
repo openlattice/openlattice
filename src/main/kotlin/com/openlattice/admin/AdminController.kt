8544fb745b805b5edb9d782d4d592bae33ad2d43
Katherine Bernstein <katherine@openlattice.com>
2019-04-24 15:33:07 -0700

    api to get entity type entity set counts + add annotations to AdminController

--- src/main/kotlin/com/openlattice/admin/AdminController.kt
+++ src/main/kotlin/com/openlattice/admin/AdminController.kt
@@ -6,11 +6,11 @@ import com.openlattice.authorization.AuthorizationManager
 import com.openlattice.authorization.AuthorizingComponent
 import com.openlattice.authorization.Principal
 import com.openlattice.authorization.Principals
+import com.openlattice.datastore.services.EdmManager
 import com.openlattice.hazelcast.HazelcastMap
 import com.openlattice.organizations.PrincipalSet
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.inject.Inject
 
@@ -27,6 +27,9 @@ class AdminController : AdminApi, AuthorizingComponent {
     @Inject
     private lateinit var hazelcast: HazelcastInstance
 
+    @Inject
+    private lateinit var edmManager: EdmManager
+
 
     @GetMapping(value = [RELOAD_CACHE])
     override fun reloadCache() {
@@ -35,18 +38,26 @@ class AdminController : AdminApi, AuthorizingComponent {
     }
 
     @GetMapping(value = [RELOAD_CACHE + NAME_PATH])
-    override fun reloadCache(name: String) {
+    override fun reloadCache(@PathVariable(NAME) name: String) {
         ensureAdminAccess()
         hazelcast.getMap<Any, Any>(HazelcastMap.valueOf(name).name).loadAll(true)
+    }
+
+    @GetMapping(value = [PRINCIPALS + ID_PATH])
+    override fun getUserPrincipals(@PathVariable(ID) principalId: String): Set<Principal> {
+        ensureAdminAccess()
+        return Principals.getUserPrincipals(principalId)
+    }
+
+    @PostMapping(value = [ENTITY_SETS + COUNT], consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE])
+    override fun countEntitySetsOfEntityTypes(@RequestBody entityTypeIds: Set<UUID>): Map<UUID, Int> {
+        ensureAdminAccess()
+        return edmManager.countEntitySetsOfEntityTypes(entityTypeIds)
     }
 
     override fun getAuthorizationManager(): AuthorizationManager {
         return authorizationManager
     }
 
-    @GetMapping(value = [PRINCIPALS + ID_PATH])
-    override fun getUserPrincipals(principalId: String): Set<Principal> {
-        ensureAdminAccess()
-        return Principals.getUserPrincipals(principalId)
-    }
 }
