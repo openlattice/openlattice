b3b6dc5cdabba0eeb53b6d355c67571f8337c555
you-leee <julia@openlattice.com>
2019-04-29 18:26:30 -0700

    LATTICE-1973 remove unused hazelcastInstance from AssemblerConnectionManager

--- src/main/java/com/openlattice/datastore/pods/DatastoreServicesPod.java
+++ src/main/java/com/openlattice/datastore/pods/DatastoreServicesPod.java
@@ -375,7 +375,6 @@ public class DatastoreServicesPod {
                 edmAuthorizationHelper(),
                 organizationsManager(),
                 dcs(),
-                hazelcastInstance,
                 eventBus,
                 metricRegistry );
     }
