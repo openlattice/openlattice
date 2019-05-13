606e4ae63d25787479801ecc0464a80b1158596e
you-leee <julia@openlattice.com>
2019-05-13 13:41:47 -0700

    create entity_set_counts view initialization task

--- src/main/kotlin/com/openlattice/data/storage/PostgresEntitySetSizesTask.kt
+++ src/main/kotlin/com/openlattice/data/storage/PostgresEntitySetSizesTask.kt
@@ -20,30 +20,18 @@
  */
 package com.openlattice.data.storage
 
-import com.openlattice.edm.set.EntitySetFlag
-import com.openlattice.postgres.PostgresColumn.*
-import com.openlattice.postgres.PostgresTable.IDS
-import com.openlattice.postgres.PostgresTable.ENTITY_SETS
+import com.openlattice.postgres.PostgresColumn.COUNT
+import com.openlattice.postgres.PostgresColumn.ENTITY_SET_ID
 import com.openlattice.tasks.HazelcastFixedRateTask
-import com.openlattice.tasks.HazelcastInitializationTask
+import com.openlattice.tasks.Task
 import org.slf4j.LoggerFactory
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 
-private const val ENTITY_SET_SIZES_TABLE = "entity_set_counts"
 private val logger = LoggerFactory.getLogger(PostgresEntitySetSizesTask::class.java)
 
 class PostgresEntitySetSizesTask : HazelcastFixedRateTask<PostgresEntitySetSizesTaskDependency> {
-    /*init {
-        logger.info("Creating entity set count views.")
-        val connection = getDependency().hikariDataSource.connection
-        connection.use {
-            it.createStatement().use { stmt ->
-                stmt.execute(CREATE_ENTITY_SET_COUNTS_VIEW)
-            }
-        }
-    }*/
 
     override fun getInitialDelay(): Long {
         return 0
@@ -67,7 +55,7 @@ class PostgresEntitySetSizesTask : HazelcastFixedRateTask<PostgresEntitySetSizes
     }
 
     override fun getName(): String {
-        return "entity_set_sizes_refresh_task"
+        return Task.POSTGRES_ENTITY_SET_SIZES_REFRESH_TASK.name
     }
 
     override fun getDependenciesClass(): Class<out PostgresEntitySetSizesTaskDependency> {
@@ -90,21 +78,6 @@ class PostgresEntitySetSizesTask : HazelcastFixedRateTask<PostgresEntitySetSizes
     }
 }
 
-private val NORMAL_ENTITY_SET_COUNTS =
-        "( SELECT ${ENTITY_SET_ID.name}, COUNT(*) as $COUNT FROM ${IDS.name} " +
-                "GROUP BY (${ENTITY_SET_ID.name}) )"
+private const val REFRESH_ENTITY_SET_COUNTS_VIEW = "REFRESH MATERIALIZED VIEW $ENTITY_SET_SIZES_VIEW"
 
-private val LINKED_ENTITY_SET_COUNTS =
-        "( SELECT  ${ID.name} as ${ENTITY_SET_ID.name}, COUNT(DISTINCT ${LINKING_ID.name}) as $COUNT FROM " +
-                "( SELECT DISTINCT ${ENTITY_SET_ID.name}, ${LINKING_ID.name} FROM ${IDS.name} WHERE ${LINKING_ID.name} IS NOT NULL ) as linking_ids " +
-                "INNER JOIN " +
-                "( SELECT ${ID.name}, ${LINKED_ENTITY_SETS.name} FROM ${ENTITY_SETS.name} WHERE '${EntitySetFlag.LINKING}' = ANY(${FLAGS.name}) ) as linking_sets " +
-                "ON ( linking_ids.${ENTITY_SET_ID.name} = ANY(linking_sets.${LINKED_ENTITY_SETS.name}) ) " +
-                "GROUP BY linking_sets.${ID.name} )"
-
-private val CREATE_ENTITY_SET_COUNTS_VIEW = "CREATE MATERIALIZED VIEW IF NOT EXISTS $ENTITY_SET_SIZES_TABLE " +
-        "AS $NORMAL_ENTITY_SET_COUNTS UNION $LINKED_ENTITY_SET_COUNTS"
-
-private const val REFRESH_ENTITY_SET_COUNTS_VIEW = "REFRESH MATERIALIZED VIEW $ENTITY_SET_SIZES_TABLE"
-
-private val GET_ENTITY_SET_COUNT = "SELECT $COUNT FROM $ENTITY_SET_SIZES_TABLE WHERE ${ENTITY_SET_ID.name} = ?"
+private val GET_ENTITY_SET_COUNT = "SELECT $COUNT FROM $ENTITY_SET_SIZES_VIEW WHERE ${ENTITY_SET_ID.name} = ?"
