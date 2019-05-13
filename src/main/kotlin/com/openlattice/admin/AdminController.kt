97f5a0a235c4100f442c2da5b473193ab11b660e
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-05-13 16:55:54 -0700

    Allow admin to debug access

--- src/main/kotlin/com/openlattice/admin/AdminController.kt
+++ src/main/kotlin/com/openlattice/admin/AdminController.kt
@@ -1,14 +1,21 @@
 package com.openlattice.admin
 
 import com.codahale.metrics.annotation.Timed
+import com.google.common.collect.Iterables
 import com.hazelcast.core.HazelcastInstance
 import com.openlattice.auditing.AuditRecordEntitySetsManager
 import com.openlattice.authorization.AuthorizationManager
 import com.openlattice.authorization.AuthorizingComponent
 import com.openlattice.authorization.Principal
 import com.openlattice.authorization.Principals
+import com.openlattice.data.storage.MetadataOption
+import com.openlattice.data.storage.selectEntitySetWithCurrentVersionOfPropertyTypes
+import com.openlattice.datastore.services.EdmManager
 import com.openlattice.edm.PostgresEdmManager
 import com.openlattice.hazelcast.HazelcastMap
+import com.openlattice.postgres.DataTables.quote
+import org.apache.olingo.commons.api.edm.EdmPrimitiveType
+import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -18,9 +25,10 @@ import javax.inject.Inject
 @RestController
 @RequestMapping(CONTROLLER)
 class AdminController : AdminApi, AuthorizingComponent {
-
     companion object {
+
         private val logger = LoggerFactory.getLogger(AdminController::class.java)!!
+
     }
 
     @Inject
@@ -34,6 +42,61 @@ class AdminController : AdminApi, AuthorizingComponent {
 
     @Inject
     private lateinit var postgresEdmManager: PostgresEdmManager
+
+    @Inject
+    private lateinit var edm: EdmManager
+
+    @GetMapping(value = [SQL + ID_PATH], produces = [MediaType.APPLICATION_JSON_VALUE])
+    override fun getEntitySetSql(
+            @PathVariable(ID) entitySetId: UUID,
+            @RequestParam(OMIT_ENTITY_SET_ID, defaultValue = "false") omitEntitySetId: Boolean
+    ): String {
+        ensureAdminAccess()
+        val entitySet = edm.getEntitySet(entitySetId)
+        return if (entitySet.isLinking) {
+            buildEntitySetSql(
+                    entitySet.linkedEntitySets.associateWith { Optional.empty<Set<UUID>>() }, true, omitEntitySetId
+            )
+        } else {
+            buildEntitySetSql(mapOf(entitySetId to Optional.empty()), false, omitEntitySetId)
+        }
+    }
+
+    @PostMapping(
+            value = [SQL],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getEntitySetSql(
+            @RequestBody entityKeyIds: Map<UUID, Optional<Set<UUID>>>,
+            @RequestParam(LINKING, defaultValue = "false") linking: Boolean,
+            @RequestParam(OMIT_ENTITY_SET_ID, defaultValue = "false") omitEntitySetId: Boolean
+    ): String {
+        ensureAdminAccess()
+        return buildEntitySetSql(entityKeyIds, linking, omitEntitySetId)
+    }
+
+    private fun buildEntitySetSql(
+            entityKeyIds: Map<UUID, Optional<Set<UUID>>>, linking: Boolean, omitEntitySetId: Boolean
+    ): String {
+        val entityTypeId = Iterables.getOnlyElement(
+                edm.getEntitySetsAsMap(entityKeyIds.keys).values.map { it.entityTypeId }.toSet()
+        )
+        val entityType = edm.getEntityType(entityTypeId)
+        val propertyTypes = edm.getPropertyTypesAsMap(entityType.properties)
+        return selectEntitySetWithCurrentVersionOfPropertyTypes(
+                entityKeyIds,
+                propertyTypes.mapValues { quote(it.value.type.fullQualifiedNameAsString) },
+                entityType.properties,
+                entityKeyIds.keys.associateWith { entityType.properties },
+                mapOf(),
+                EnumSet.allOf(MetadataOption::class.java),
+                propertyTypes.mapValues { it.value.datatype == EdmPrimitiveTypeKind.Binary },
+                linking,
+                omitEntitySetId
+        )
+
+    }
 
 
     @Timed
@@ -65,8 +128,10 @@ class AdminController : AdminApi, AuthorizingComponent {
     }
 
     @Timed
-    @PostMapping(value = [ENTITY_SETS + COUNT], consumes = [MediaType.APPLICATION_JSON_VALUE],
-            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(
+            value = [ENTITY_SETS + COUNT], consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
     override fun countEntitySetsOfEntityTypes(@RequestBody entityTypeIds: Set<UUID>): Map<UUID, Long> {
         ensureAdminAccess()
         return postgresEdmManager.countEntitySetsOfEntityTypes(entityTypeIds)
