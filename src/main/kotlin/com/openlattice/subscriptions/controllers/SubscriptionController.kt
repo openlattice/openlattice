ce67d08329dee6d0f77a4b37911f98c80885deaf
Drew <drew@openlattice.com>
2019-06-05 12:29:06 -0700

    add controller annotations

--- src/main/kotlin/com/openlattice/subscriptions/controllers/SubscriptionController.kt
+++ src/main/kotlin/com/openlattice/subscriptions/controllers/SubscriptionController.kt
@@ -1,11 +1,13 @@
 
 package com.openlattice.subscriptions.controllers
 
+import com.codahale.metrics.annotation.Timed
 import com.openlattice.authorization.AuthorizationManager
 import com.openlattice.authorization.AuthorizingComponent
 import com.openlattice.data.EntityDataKey
 import com.openlattice.subscriptions.SubscriptionApi
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import javax.inject.Inject
 
@@ -17,18 +19,26 @@ constructor(
         private val authorizationManager: AuthorizationManager
 ) : SubscriptionApi, AuthorizingComponent {
 
+    @Timed
+    @RequestMapping(path = [SubscriptionApi.BASE], method = [RequestMethod.PUT])
     override fun addSubscription(entity: EntityDataKey): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    @Timed
+    @RequestMapping(path = [SubscriptionApi.BASE], method = [RequestMethod.POST])
     override fun updateSubscription(entity: EntityDataKey): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    @Timed
+    @RequestMapping(path = [SubscriptionApi.BASE], method = [RequestMethod.DELETE])
     override fun deleteSubscription(entity: EntityDataKey): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    @Timed
+    @RequestMapping(path = [SubscriptionApi.BASE], method = [RequestMethod.GET])
     override fun getAllSubscriptions(): Iterable<Int> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
