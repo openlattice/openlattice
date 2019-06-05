1fe962e56db80361e26a8ba76587a06d65807225
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-06-05 13:13:19 -0700

    Update Graph Api

---
+++ api/src/main/kotlin/com/openlattice/graph/NeighborhoodFilter.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
+package com.openlattice.graph
+
+import com.openlattice.analysis.requests.Filter
+import java.util.*
+
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+data class NeighborhoodFilter(
+        val entityTypeId: Optional<UUID>,
+        val entitySetIds: Optional<Set<UUID>>,
+        val associationTypeId: Optional<UUID>,
+        val associationEntitySetIds: Optional<Set<UUID>>,
+        val entityFilters: Optional<Set<Filter>>,
+        val associationFilters: Optional<Set<Filter>>
+) {
+    init{
+        check( entityTypeId.isPresent xor entitySetIds.isPresent ) {
+            "Either entity entity type id or entity set ids must be present."
+        }
+
+        check( associationTypeId.isPresent xor associationEntitySetIds.isPresent ) {
+            "Either asssociation entity type id or entity set ids must be present."
+        }
+    }
+}
