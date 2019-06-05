1c2be44df6da680e5dcf88eb0a95f27c47077ec9
Drew <drew@openlattice.com>
2019-06-05 12:13:26 -0700

    Add an initial subscriptionAPI to API and subscriptionController to datastore

---
+++ api/src/main/kotlin/SubscriptionApi.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
+package com.openlattice.subscriptions
+
+import com.openlattice.data.EntityDataKey
+import retrofit2.http.*
+
+/**
+ * This API is for managing Subscriptions on entities
+ */
+interface SubscriptionApi {
+    companion object {
+        const val SERVICE = "/datastore"
+        const val CONTROLLER = "/subscriptions"
+        const val BASE = SERVICE + CONTROLLER
+    }
+
+    /**
+     * Creates a subscription
+     */
+    @PUT(BASE)
+    fun addSubscription(@Body entity: EntityDataKey): Int
+
+    /**
+     * Updates a subscription
+     */
+    @POST(BASE )
+    fun updateSubscription(@Body entity: EntityDataKey): Int
+
+    /**
+     * Removes a subscription
+     */
+    @HTTP(method = "DELETE", path = BASE, hasBody = true)
+    fun deleteSubscription(@Body entity: EntityDataKey): Int
+
+    /**
+     * Returns all subscriptions
+     */
+    @GET(BASE)
+    fun getAllSubscriptions(): Iterable<Int>
+}
