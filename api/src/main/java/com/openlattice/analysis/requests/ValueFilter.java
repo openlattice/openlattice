b6e7cf3acb7fc630aaa4c395944536a54ae5ab5a
Drew <drew@openlattice.com>
2019-06-06 15:50:29 -0700

    fix serialization

--- api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
+++ api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
@@ -1,12 +1,17 @@
 package com.openlattice.analysis.requests;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ValueFilter<T extends Comparable<T>> implements Filter {
+    @JsonProperty("values")
     private final Set<T> values;
 
-    public ValueFilter( Set<T> values ) {
+    @JsonCreator
+    public ValueFilter( @JsonProperty("values") Set<T> values ) {
         this.values = values;
     }
 
