03ef3fd82b94323151a33b05e94571d81c9b8691
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-06-06 16:38:50 -0700

    Clean up controller

--- src/main/kotlin/com/openlattice/graph/controllers/GraphController.kt
+++ src/main/kotlin/com/openlattice/graph/controllers/GraphController.kt
@@ -22,17 +22,13 @@
 package com.openlattice.graph.controllers
 
 import com.codahale.metrics.annotation.Timed
-import com.google.common.collect.ListMultimap
-import com.google.common.collect.SetMultimap
 import com.openlattice.authorization.*
 import com.openlattice.controllers.exceptions.ForbiddenException
 import com.openlattice.data.requests.NeighborEntityDetails
 import com.openlattice.datastore.services.EdmManager
-import com.openlattice.edm.EntitySet
 import com.openlattice.edm.type.PropertyType
 import com.openlattice.graph.*
-import com.openlattice.graph.query.GraphQuery
-import com.openlattice.graph.query.GraphQueryState
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.inject.Inject
@@ -52,12 +48,21 @@ constructor(
 //        private val filteredAggregation
 ) : GraphApi, AuthorizingComponent {
     @Timed
+    @PostMapping(
+            value = [NEIGHBORS + ENTITY_SET_ID_PATH],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+
     override fun neighborhoodQuery(
             entitySetId: UUID,
             query: NeighborhoodQuery
     ): Map<UUID, List<NeighborEntityDetails>> {
         val entitySetsById = graphQueryService.getEntitySetForIds(query.ids)
-        val (allEntitySetIds, requiredPropertyTypes) = getEntitySets(query, entitySetsById.values)
+        val (allEntitySetIds, requiredPropertyTypes) = resolveEntitySetIdsAndRequiredAuthorizations(
+                query,
+                entitySetsById.values
+        )
 
         /*
          * We need to figure out what property types are authorized for all entity sets
@@ -71,87 +76,30 @@ constructor(
 
         ensureReadOnRequired(authorizedPropertyTypes, requiredPropertyTypes)
 
-
-        graphQueryService.submitQuery(query)
+        val propertyTypes = authorizedPropertyTypes.values.flatMap { it.values }.associateBy { it.id }
+        graphQueryService.submitQuery(query, propertyTypes, authorizedPropertyTypes.mapValues { it.value.keys })
         return mapOf()
     }
 
-
-    @Timed
-//    @PostMapping(
-//            value = QUERY,
-//            consumes = [MediaType.APPLICATION_JSON_VALUE],
-//            produces = [MediaType.APPLICATION_JSON_VALUE]
-//    )
-    override fun submit(query: SimpleGraphQuery): GraphQueryState {
-        //Collect the data to authorize
-
-        //Collect the things to perserve
-//        return graphQueryService.submitQuery(query);
-        TODO("Not implemented")
-    }
-
-    @Timed
-//    @PostMapping(
-//            value = QUERY + ID_PATH,
-//            consumes = [MediaType.APPLICATION_JSON_VALUE],
-//            produces = [MediaType.APPLICATION_JSON_VALUE]
-//    )
-    override fun getQueryState(
-            @PathVariable(ID) queryId: UUID,
-            @RequestBody options: Set<GraphQueryState.Option>
-    ): GraphQueryState {
-        return graphQueryService.getQueryState(queryId, options)
-    }
-
-    override fun getQueryState(queryId: UUID): GraphQueryState {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    @Timed
-    override fun getResults(queryId: UUID): SubGraph {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    @Timed
-    override fun graphQuery(ops: GraphQuery): ListMultimap<UUID, SetMultimap<UUID, SetMultimap<UUID, Any>>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-    private fun getEntitySets(selection: NeighborhoodSelection): List<UUID> {
-        return selection.outgingEntityTypeIds
-                .map(edm::getEntitySetsOfType).orElseGet { emptyList() }
-                .map(EntitySet::getId) +
-                selection.outgingEntitySetIds.orElseGet { emptySet() } +
-                selection.incomingEntityTypeIds
-                        .map(edm::getEntitySetsOfType).orElseGet { emptyList() }
-                        .map(EntitySet::getId) +
-                selection.incomingEntitySetIds.orElseGet { emptySet() } +
-                selection.associationTypeIds
-                        .map(edm::getEntitySetsOfType).orElseGet { emptyList() }
-                        .map(EntitySet::getId) +
-                selection.associationEntitySetIds.orElseGet { emptySet() }
-    }
-
     private fun getRequiredAuthorizations(selection: NeighborhoodSelection): Map<UUID, Set<UUID>> {
-        return selection.outgoingEntityFilters.map { filters -> filters.mapValues { it.value.keys } }.orElseGet { emptyMap() } +
-                selection.incomingEntityFilters.map { filters -> filters.mapValues { it.value.keys } }.orElseGet { emptyMap() } +
+        return selection.entityFilters.map { filters -> filters.mapValues { it.value.keys } }.orElseGet { emptyMap() } +
                 selection.associationFilters.map { filters -> filters.mapValues { it.value.keys } }.orElseGet { emptyMap() }
     }
 
-    private fun getEntitySets(
+    private fun resolveEntitySetIdsAndRequiredAuthorizations(
             query: NeighborhoodQuery, baseEntitySetIds: Collection<UUID>
     ): Pair<Set<UUID>, Map<UUID, Set<UUID>>> {
         val requiredAuthorizations: MutableMap<UUID, MutableSet<UUID>> = mutableMapOf()
-        val allEntitySetIds = baseEntitySetIds +
-                query.selections.flatMap {
-                    getRequiredAuthorizations(it).forEach { entitySetId, requiredPropertyTypeIds ->
+        val allEntitySetIds = baseEntitySetIds.asSequence() +
+                (query.srcSelections.asSequence() + query.dstSelections.asSequence()).flatMap { selection ->
+                    getRequiredAuthorizations(selection).forEach { entitySetId, requiredPropertyTypeIds ->
                         requiredAuthorizations
                                 .getOrPut(entitySetId) { mutableSetOf() }
                                 .addAll(requiredPropertyTypeIds)
                     }
-                    getEntitySets(it)
+                    //
+                    graphQueryService.getEntitySets(selection.entityTypeIds).asSequence() +
+                            graphQueryService.getEntitySets(selection.associationTypeIds).asSequence()
                 }
 
         return allEntitySetIds.toSet() to requiredAuthorizations
