3b390a6c7777113ebf1edee0a223f4a4fd1eadae
Drew <drew@openlattice.com>
2019-06-06 21:57:31 -0700

    V.Inefficient life

--- src/main/kotlin/com/openlattice/subscriptions/controllers/FeedsController.kt
+++ src/main/kotlin/com/openlattice/subscriptions/controllers/FeedsController.kt
@@ -1,15 +1,13 @@
 package com.openlattice.subscriptions.controllers
 
 import com.codahale.metrics.annotation.Timed
-import com.openlattice.auditing.AuditEventType
+import com.openlattice.analysis.requests.Filter
+import com.openlattice.analysis.requests.WrittenTwoWeeksFilter
 import com.openlattice.authorization.AuthorizationManager
 import com.openlattice.authorization.AuthorizingComponent
 import com.openlattice.authorization.EdmAuthorizationHelper
 import com.openlattice.authorization.Principals
-import com.openlattice.data.EntityDataKey
-import com.openlattice.graph.GraphApi
-import com.openlattice.graph.GraphQueryService
-import com.openlattice.subscriptions.FeedEntry
+import com.openlattice.graph.*
 import com.openlattice.subscriptions.FeedsApi
 import com.openlattice.subscriptions.SubscriptionService
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,23 +30,49 @@ constructor(
 
     @Timed
     @RequestMapping(path = ["", "/"], method = [RequestMethod.GET])
-    override fun getLatestFeed(): Iterator<FeedEntry> {
-
-        //Ret EDK + eventType + EDKs + Principal
-        val allSubscriptions = subscriptionService.getAllSubscriptions(Principals.getCurrentUser())
-
-//        authzHelper.getAuthorizedPropertiesOnEntitySets( authorizedEntitySetIds, READ_PERMISSION, Principals.getCurrentPrincipals() );
-//        authzHelper.getAuthorizedPropertiesOnEntitySets(entitySetIds, EnumSet.of( Permission.READ ), setOf( organizationPrincipal.getPrincipal() ) );
-
-//        allSubscriptions.forEach { sub ->
-//            graphQueryService.submitQuery(sub, )
-//        }
-
-        return allSubscriptions.map { return@map FeedEntry(EntityDataKey(UUID.randomUUID(), UUID.randomUUID()), AuditEventType.CREATE_ENTITY_TYPE, setOf(), Principals.getCurrentUser()) }.listIterator()
+    override fun getLatestFeed(): Iterator<Neighborhood> {
+        return subscriptionService.getAllSubscriptions(Principals.getCurrentUser()).map { neighborhoodQuery ->
+            val newSrcList = rebuildSelectionList( neighborhoodQuery.srcSelections )
+            val newDstList = rebuildSelectionList( neighborhoodQuery.dstSelections )
+            graphApi.neighborhoodQuery(
+                    UUID.randomUUID(),
+                    NeighborhoodQuery(neighborhoodQuery.ids, newSrcList, newDstList))
+        }.listIterator()
     }
 
     override fun getAuthorizationManager(): AuthorizationManager {
         return authorizationManager
     }
+
+    fun insertRecencyFilter( filtersMap: Optional<Map<UUID, Map<UUID, Set<Filter>>>> ) : Optional<Map<UUID, Map<UUID, Set<Filter>>>> {
+        val newEntityFilterMap = mutableMapOf<UUID, Map<UUID, Set<Filter>>>()
+        filtersMap.get().forEach {
+            val newFilterMap = mutableMapOf<UUID, Set<Filter>>()
+            it.value.forEach {
+                val newFilterSet = it.value.plus(WrittenTwoWeeksFilter())
+                newFilterMap.put(it.key, newFilterSet)
+            }
+            newEntityFilterMap.put( it.key, newFilterMap )
+        }
+        return Optional.of(newEntityFilterMap)
+    }
+
+    fun rebuildSelectionList( targetList: List<NeighborhoodSelection>) : List<NeighborhoodSelection> {
+        val newList = mutableListOf<NeighborhoodSelection>()
+        targetList.forEachIndexed { index, neighborhoodSelection ->
+            val newEntityFilterMap = insertRecencyFilter(neighborhoodSelection.entityFilters)
+            val newAssociationFilterMap = insertRecencyFilter(neighborhoodSelection.associationFilters)
+
+            newList.add( NeighborhoodSelection(
+                    neighborhoodSelection.entityTypeIds,
+                    neighborhoodSelection.entitySetIds,
+                    newEntityFilterMap,
+                    neighborhoodSelection.associationTypeIds,
+                    neighborhoodSelection.associationEntitySetIds,
+                    newAssociationFilterMap))
+        }
+        return newList
+    }
+
 }
 
