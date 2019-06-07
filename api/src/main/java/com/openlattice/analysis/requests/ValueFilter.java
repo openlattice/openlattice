d857a44174df70425d498c19c82dfd00415ee737
Drew <drew@openlattice.com>
2019-06-06 20:31:35 -0700

    forget about valuefilter and add recentlywrittenfilter

--- api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
+++ api/src/main/java/com/openlattice/analysis/requests/ValueFilter.java
@@ -1,17 +1,12 @@
 package com.openlattice.analysis.requests;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ValueFilter<T extends Comparable<T>> implements Filter {
-    @JsonProperty("values")
     private final Set<T> values;
 
-    @JsonCreator
-    public ValueFilter( @JsonProperty("values") Set<T> values ) {
+    public ValueFilter( Set<T> values ) {
         this.values = values;
     }
 
---
+++ api/src/main/java/com/openlattice/analysis/requests/WrittenTwoWeeksFilter.kt
@@ -0,0 +1,22 @@
+package com.openlattice.analysis.requests
+
+import java.time.OffsetDateTime
+
+class WrittenTwoWeeksFilter: AbstractRangeFilter<OffsetDateTime>( OffsetDateTime.now().minusDays(14), true, OffsetDateTime.MAX, true) {
+    override fun getLowerboundSql(): String {
+        return lowerbound.toString()
+    }
+
+    override fun getUpperboundSql(): String {
+        return upperbound.toString()
+    }
+
+    override fun getMaxValue(): OffsetDateTime {
+        return OffsetDateTime.MAX
+    }
+
+    override fun getMinValue(): OffsetDateTime {
+        return OffsetDateTime.MIN
+    }
+
+}
