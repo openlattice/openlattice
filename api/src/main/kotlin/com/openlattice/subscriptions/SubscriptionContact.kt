1fdc15c11547a5b79f3cf55ec5f001bde847a9f2
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-06-07 14:20:44 -0700

    Subscription contacts.

--- api/src/main/kotlin/com/openlattice/subscriptions/SubscriptionContact.kt
+++ api/src/main/kotlin/com/openlattice/subscriptions/SubscriptionContact.kt
@@ -1,9 +1,15 @@
 package com.openlattice.subscriptions
 
 import com.openlattice.graph.NeighborhoodQuery
+import java.time.OffsetDateTime
+
+enum class SubscriptionContactType {
+    EMAIL,
+    PHONE
+}
 
 data class SubscriptionContact (
         val subscription: NeighborhoodQuery,
-        val type: String,
-        val contact: String
+        val contact : Map<SubscriptionContactType, String>,
+        val lastNotify : OffsetDateTime = OffsetDateTime.now().minusYears(100)
 )
