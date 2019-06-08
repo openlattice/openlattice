2eaa97e0df24b90e1c310c79d658ec50603d2eb0
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-06-07 18:24:50 -0700

    woops

--- src/main/kotlin/com/openlattice/hazelcast/serializers/SubscriptionNotificationTaskStreamSerializer.kt
+++ src/main/kotlin/com/openlattice/hazelcast/serializers/SubscriptionNotificationTaskStreamSerializer.kt
@@ -4,6 +4,7 @@ import com.hazelcast.nio.ObjectDataInput
 import com.hazelcast.nio.ObjectDataOutput
 import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer
 import com.openlattice.hazelcast.StreamSerializerTypeIds
+import com.openlattice.subscriptions.SubscriptionNotificationTask
 import org.springframework.stereotype.Component
 
 /**
@@ -11,21 +12,21 @@ import org.springframework.stereotype.Component
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 @Component
-class SubscriptionNotificationTaskStreamSerializer : SelfRegisteringStreamSerializer<SubscriptionNotificationTaskStreamSerializer> {
+class SubscriptionNotificationTaskStreamSerializer : SelfRegisteringStreamSerializer<SubscriptionNotificationTask> {
     override fun getTypeId(): Int {
         return StreamSerializerTypeIds.SUBSCRIPTION_NOTIFICATION_TASK.ordinal
     }
 
-    override fun getClazz(): Class<out SubscriptionNotificationTaskStreamSerializer> {
-        return SubscriptionNotificationTaskStreamSerializer::class.java
+    override fun getClazz(): Class<out SubscriptionNotificationTask> {
+        return SubscriptionNotificationTask::class.java
     }
 
-    override fun write(out: ObjectDataOutput?, `object`: SubscriptionNotificationTaskStreamSerializer?) {
+    override fun write(out: ObjectDataOutput?, `object`: SubscriptionNotificationTask?) {
 
     }
 
-    override fun read(`in`: ObjectDataInput?): SubscriptionNotificationTaskStreamSerializer {
-        return SubscriptionNotificationTaskStreamSerializer()
+    override fun read(`in`: ObjectDataInput?): SubscriptionNotificationTask {
+        return SubscriptionNotificationTask()
     }
 
 }
