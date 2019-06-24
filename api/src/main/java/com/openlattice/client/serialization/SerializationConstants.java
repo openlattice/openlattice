b062b7379c1de3ebf327a3668005e866a49dc5dd
jokedurnez <joke.durnez@gmail.com>
2019-06-24 11:42:56 -0700

    add objects for entitytypepropertymetadata

--- api/src/main/java/com/openlattice/client/serialization/SerializationConstants.java
+++ api/src/main/java/com/openlattice/client/serialization/SerializationConstants.java
@@ -192,6 +192,7 @@ public final class SerializationConstants {
     public static final String PROPERTY_FIELD                   = "property";
     public static final String PROPERTY_METADATA                = "propertyMetadata";
     public static final String PROPERTY_TAGS                    = "propertyTags";
+    public static final String PROPERTY_TYPE_DESCRIPTION        = "propertyTypeDescription";
     public static final String PROPERTY_TYPES                   = "propertyTypes";
     public static final String PROPERTY_TYPE_ID                 = "propertyTypeId";
     public static final String PROPERTY_TYPE_IDS                = "propertyTypeIds";
