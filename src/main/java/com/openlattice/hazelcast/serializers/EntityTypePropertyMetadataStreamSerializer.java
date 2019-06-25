17d22ca10f624989d4cf4811f5848be7651f9d25
jokedurnez <joke.durnez@gmail.com>
2019-06-24 18:12:08 -0700

    serializers

---
+++ src/main/java/com/openlattice/hazelcast/serializers/EntityTypePropertyMetadataStreamSerializer.java
@@ -0,0 +1,44 @@
+package com.openlattice.hazelcast.serializers;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.kryptnostic.rhizome.hazelcast.serializers.SetStreamSerializers;
+import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
+import com.openlattice.edm.set.EntitySetPropertyMetadata;
+import com.openlattice.edm.type.EntityTypePropertyMetadata;
+import com.openlattice.hazelcast.StreamSerializerTypeIds;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class EntityTypePropertyMetadataStreamSerializer
+        implements SelfRegisteringStreamSerializer<EntityTypePropertyMetadata> {
+
+    @Override
+    public void write( ObjectDataOutput out, EntityTypePropertyMetadata object ) throws IOException {
+        out.writeUTF( object.getTitle() );
+        out.writeUTF( object.getDescription() );
+    }
+
+    @Override
+    public EntityTypePropertyMetadata read( ObjectDataInput in ) throws IOException {
+        String title = in.readUTF();
+        String description = in.readUTF();
+        return new EntityTypePropertyMetadata( title, description );
+    }
+
+    @Override
+    public int getTypeId() {
+        return StreamSerializerTypeIds.ENTITY_TYPE_PROPERTY_METADATA.ordinal();
+    }
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public Class<? extends EntityTypePropertyMetadata> getClazz() {
+        return EntityTypePropertyMetadata.class;
+    }
+
+}
