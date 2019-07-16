852ad1daa2db30d65e6da6de7a9e90d876c8f1b9
you-leee <julia@openlattice.com>
2019-07-16 11:11:16 -0700

    make it compile + rename

--- src/main/kotlin/com/openlattice/analysis/assembler/AssemblyAnalyzationController.kt
+++ src/main/kotlin/com/openlattice/analysis/assembler/AssemblyAnalyzationController.kt
@@ -61,7 +61,7 @@ class AssemblyAnalyzationController : AssemblyAnalyzationApi, AuthorizingCompone
     @PostMapping(value = [SIMPLE_AGGREGATION], produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun getSimpleAssemblyAggregates(
             @RequestBody assemblyAggregationFilter: AssemblyAggregationFilter
-    ): Iterable<Map<String, Any>> {
+    ): Iterable<Map<String, Any?>> {
         val principal = PostgresRoles.buildPostgresUsername(Principals.getCurrentSecurablePrincipal())
         val account = MaterializedViewAccount(principal, dbCredService.getDbCredential(principal))
 
@@ -115,7 +115,7 @@ class AssemblyAnalyzationController : AssemblyAnalyzationApi, AuthorizingCompone
 
 
         val connection = assemblerConnectionManager.connect(dbName, account).connection
-        val asd = assemblerQueryService.simpleAggregation(
+        val aggregationValues = assemblerQueryService.simpleAggregation(
                 connection,
                 srcEntitySetName, edgeEntitySetName, dstEntitySetName,
                 srcGroupColumns, edgeGroupColumns, dstGroupColumns,
@@ -123,8 +123,7 @@ class AssemblyAnalyzationController : AssemblyAnalyzationApi, AuthorizingCompone
                 assemblyAggregationFilter.customCalculations,
                 srcFilters, edgeFilters, dstFilters)
 
-        return asd
-
+        return aggregationValues
     }
 
     override fun getAuthorizationManager(): AuthorizationManager {
