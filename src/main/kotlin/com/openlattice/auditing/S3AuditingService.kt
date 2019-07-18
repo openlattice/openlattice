e4053cb7336968a23844b5c931cd80f315234d07
Katherine Bernstein <katherine@openlattice.com>
2019-07-18 12:08:35 -0700

    more logging

--- src/main/kotlin/com/openlattice/auditing/S3AuditingService.kt
+++ src/main/kotlin/com/openlattice/auditing/S3AuditingService.kt
@@ -39,10 +39,12 @@ class S3AuditingService(
     private val s3 = newS3Client(auditingConfiguration.awsS3ClientConfiguration)
 
     private val refreshQueue = executorService.execute {
-        while(true) {
-            longIdService
+        logger.info("Executing refreshQueue")
+        while (true) {
+            val lr = longIdService
                     .getIds(IdScopes.AUDITING.name, LONG_IDS_BATCH_SIZE.toLong())
-                    .forEach(longIdsQueue::put)
+            logger.info("Got a LongRange from longIdService")
+            lr.forEach(longIdsQueue::put)
         }
     }
 
@@ -56,31 +58,31 @@ class S3AuditingService(
         metadata.contentType = MediaType.APPLICATION_JSON_VALUE
 
         logger.info("Going to get id")
-        val partition = getId() % partitions.toLong()
-        logger.info("Going to get id again for key")
-        val key = "$partition/${System.currentTimeMillis()}/event-${getId()}.json"
+        val id = getId()
+        val partition = id % partitions.toLong()
+        val key = "$partition/${System.currentTimeMillis()}/event-$id.json"
         val s = Stopwatch.createStarted()
         logger.info("About to audit to s3")
-        s3.putObject(PutObjectRequest(bucket, key,eventsInputStream, metadata))
+        s3.putObject(PutObjectRequest(bucket, key, eventsInputStream, metadata))
         logger.info("Auditing took {} ms", s.elapsed(TimeUnit.MILLISECONDS))
         return events.size
     }
 
     fun getRecordedEvents(): List<AuditableEvent> {
         //TODO: Need to make sure that auditable event is only queued up once from s3
-        
 
-        val objectListing = s3.listObjectsV2(ListObjectsV2Request().withBucketName(bucket) )
 
-        val objects : List<AuditableEvent> = objectListing.objectSummaries.flatMap { objSummary ->
-            val someObjects: List<AuditableEvent>  = mapper.readValue(s3.getObject( bucket, objSummary.key).objectContent)
+        val objectListing = s3.listObjectsV2(ListObjectsV2Request().withBucketName(bucket))
+
+        val objects: List<AuditableEvent> = objectListing.objectSummaries.flatMap { objSummary ->
+            val someObjects: List<AuditableEvent> = mapper.readValue(s3.getObject(bucket, objSummary.key).objectContent)
             return@flatMap someObjects
         }
 
         return objects
     }
 
-    fun deleteIntegratedEvents( keys: List<String>) {
+    fun deleteIntegratedEvents(keys: List<String>) {
         TODO("Implement deleting events after they are stored.")
     }
 
