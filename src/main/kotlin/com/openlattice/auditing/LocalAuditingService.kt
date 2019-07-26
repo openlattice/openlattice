9c95c9d0894655cbd0f52ab21153780e1aba4f73
Katherine Bernstein <katherine@openlattice.com>
2019-07-25 19:50:52 -0700

    Add LocalAuditingService which doesn't require a s3 configuration for local development

---
+++ src/main/kotlin/com/openlattice/auditing/LocalAuditingService.kt
@@ -0,0 +1,104 @@
+package com.openlattice.auditing
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.common.collect.ArrayListMultimap
+import com.google.common.collect.ImmutableMap
+import com.openlattice.data.DataEdge
+import com.openlattice.data.DataGraphManager
+import com.openlattice.data.EntityDataKey
+import java.util.*
+
+class LocalAuditingService(
+        private val dataGraphService: DataGraphManager,
+        private val ares: AuditRecordEntitySetsManager,
+        private val mapper: ObjectMapper
+) : AuditingManager {
+
+    fun recordEvent(event: AuditableEvent): Int {
+        return recordEvents(listOf(event))
+    }
+
+    override fun recordEvents(events: List<AuditableEvent>): Int {
+
+        val auditingConfiguration = ares.auditingTypes
+
+        return if (auditingConfiguration.isAuditingInitialized()) {
+            events
+                    .groupBy { ares.getActiveAuditEntitySetIds(it.aclKey, it.eventType) }
+                    .filter { (auditEntitySetConfiguration, _) ->
+                        auditEntitySetConfiguration.auditRecordEntitySet != null
+                    }
+                    .map { (auditEntitySetConfiguration, entities) ->
+                        val auditEntitySet = auditEntitySetConfiguration.auditRecordEntitySet
+                        val (entityKeyIds, _) = dataGraphService.createEntities(
+                                auditEntitySet!!,
+                                toMap(entities),
+                                auditingConfiguration.propertyTypes
+                        )
+
+                        if (auditEntitySetConfiguration.auditEdgeEntitySet != null) {
+                            val auditEdgeEntitySet = auditEntitySetConfiguration.auditEdgeEntitySet
+
+                            val lm = ArrayListMultimap.create<UUID, DataEdge>()
+                            entityKeyIds.asSequence().zip(entities.asSequence())
+                                    .filter { it.second.entities.isPresent }
+                                    .forEach { (auditEntityKeyId, ae) ->
+                                        val aeEntitySetId = ae.aclKey[0]
+                                        val aeEntityKeyIds = ae.entities.get()
+                                        aeEntityKeyIds.forEach { id ->
+                                            lm.put(
+                                                    auditEdgeEntitySet,
+                                                    DataEdge(
+                                                            EntityDataKey(aeEntitySetId, id),
+                                                            EntityDataKey(auditEntitySet, auditEntityKeyId),
+                                                            ImmutableMap.of()
+                                                    )
+                                            )
+                                            return@forEach
+                                        }
+                                    }
+                            dataGraphService
+                                    .createAssociations(lm, ImmutableMap.of(auditEdgeEntitySet, emptyMap()))
+
+                        }
+                        entityKeyIds.size
+                    }.sum()
+        } else {
+            0
+        }
+    }
+
+    private fun toMap(events: List<AuditableEvent>): List<Map<UUID, Set<Any>>> {
+        val auditingConfiguration = ares.auditingTypes
+        return events.map { event ->
+            val eventEntity = mutableMapOf<UUID, Set<Any>>()
+
+            eventEntity[auditingConfiguration.getPropertyTypeId(
+                    AuditProperty.ACL_KEY
+            )] = setOf(event.aclKey.index)
+
+            event.entities.ifPresent {
+                eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.ENTITIES)] = it
+            }
+
+            event.operationId.ifPresent {
+                eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.OPERATION_ID)] = setOf(it)
+            }
+
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.ID)] = setOf(
+                    event.aclKey.last().toString()
+            ) //ID of securable object
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.PRINCIPAL)] = setOf(
+                    event.principal.toString()
+            )
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.EVENT_TYPE)] = setOf(event.eventType.name)
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.DESCRIPTION)] = setOf(event.description)
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.DATA)] = setOf(
+                    mapper.writeValueAsString(event.data)
+            )
+            eventEntity[auditingConfiguration.getPropertyTypeId(AuditProperty.TIMESTAMP)] = setOf(event.timestamp)
+
+            return@map eventEntity
+        }
+    }
+}
