04ef7c2520b0184e0bc5f07900da8ef121803c08
Katherine Bernstein <katherine@openlattice.com>
2019-07-29 13:50:18 -0700

    unreasonably small batch size

--- indexer/src/main/kotlin/com/openlattice/indexing/BackgroundIndexingService.kt
+++ indexer/src/main/kotlin/com/openlattice/indexing/BackgroundIndexingService.kt
@@ -72,7 +72,7 @@ class BackgroundIndexingService(
 ) {
     companion object {
         private val logger = LoggerFactory.getLogger(BackgroundIndexingService::class.java)!!
-        const val INDEX_SIZE = 16000
+        const val INDEX_SIZE = 1000
     }
 
     private val propertyTypes: IMap<UUID, PropertyType> = hazelcastInstance.getMap(HazelcastMap.PROPERTY_TYPES.name)
