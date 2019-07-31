704cd96fedc6f8815f607c37d56c6953e6a0bdd0
you-leee <julia@openlattice.com>
2019-07-30 18:27:40 -0700

    LATTICE-2057 add PartitionManager to Assembler

--- src/main/java/com/openlattice/datastore/pods/DatastoreServicesPod.java
+++ src/main/java/com/openlattice/datastore/pods/DatastoreServicesPod.java
@@ -271,6 +271,7 @@ public class DatastoreServicesPod {
                 authorizationManager(),
                 edmAuthorizationHelper(),
                 principalService(),
+                partitionManager(),
                 metricRegistry,
                 hazelcastInstance,
                 eventBus
