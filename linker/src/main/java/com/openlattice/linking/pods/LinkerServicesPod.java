f3eb0f5dcb7d5aaace4871c4b7fbe77ff5318d2a
you-leee <julia@openlattice.com>
2019-07-30 18:28:12 -0700

    LATTICE-2057 add PartitionManager to Assembler

--- linker/src/main/java/com/openlattice/linking/pods/LinkerServicesPod.java
+++ linker/src/main/java/com/openlattice/linking/pods/LinkerServicesPod.java
@@ -178,6 +178,7 @@ public class LinkerServicesPod {
                 authorizationManager(),
                 edmAuthorizationHelper(),
                 principalService(),
+                partitionManager(),
                 metricRegistry,
                 hazelcastInstance,
                 eventBus
