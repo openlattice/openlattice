5b0ce90b5447902c715d7b025e22ae268ac84a70
Katherine Bernstein <katherine@openlattice.com>
2019-08-05 12:09:43 -0700

    mootable

--- api/src/main/kotlin/com/openlattice/collections/EntitySetCollection.kt
+++ api/src/main/kotlin/com/openlattice/collections/EntitySetCollection.kt
@@ -16,7 +16,7 @@ class EntitySetCollection(
         title: String,
         description: Optional<String>,
         val entityTypeCollectionId: UUID,
-        var template: Map<UUID, UUID>,
+        var template: MutableMap<UUID, UUID>,
         var contacts: Set<String>,
         var organizationId: UUID
 ) : AbstractSecurableObject(id, title, description) {
@@ -28,7 +28,7 @@ class EntitySetCollection(
             @JsonProperty(SerializationConstants.TITLE_FIELD) title: String,
             @JsonProperty(SerializationConstants.DESCRIPTION_FIELD) description: Optional<String>,
             @JsonProperty(SerializationConstants.ENTITY_TYPE_COLLECTION_ID) entityTypeCollectionId: UUID,
-            @JsonProperty(SerializationConstants.TEMPLATE) template: Map<UUID, UUID>,
+            @JsonProperty(SerializationConstants.TEMPLATE) template: MutableMap<UUID, UUID>,
             @JsonProperty(SerializationConstants.CONTACTS) contacts: Set<String>,
             @JsonProperty(SerializationConstants.ORGANIZATION_ID) organizationId: Optional<UUID>
     ) : this(id, name, title, description, entityTypeCollectionId, template, contacts, organizationId.orElse(IdConstants.GLOBAL_ORGANIZATION_ID.id))
@@ -39,7 +39,7 @@ class EntitySetCollection(
             title: String,
             description: Optional<String>,
             entityTypeCollectionId: UUID,
-            template: Map<UUID, UUID>,
+            template: MutableMap<UUID, UUID>,
             contacts: Set<String>,
             organizationId: Optional<UUID>
     ) : this(Optional.of<UUID>(id), name, title, description, entityTypeCollectionId, template, contacts, organizationId)
