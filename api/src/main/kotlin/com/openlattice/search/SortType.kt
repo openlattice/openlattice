3576cb9c75acf0b443c49e625397897b2f119ba8
Katherine Bernstein <katherine@openlattice.com>
2019-08-21 14:35:43 -0700

    constrain allowed datatypes

--- api/src/main/kotlin/com/openlattice/search/SortType.kt
+++ api/src/main/kotlin/com/openlattice/search/SortType.kt
@@ -1,7 +1,47 @@
 package com.openlattice.search
 
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableSet
+import org.apache.olingo.commons.api.edm.EdmPrimitiveType
+import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind
+
+private val ALLOWED_FIELD_DATATYPES = ImmutableSet.of(
+        /* dates and times */
+        EdmPrimitiveTypeKind.Date,
+        EdmPrimitiveTypeKind.DateTimeOffset,
+        EdmPrimitiveTypeKind.TimeOfDay,
+
+        /* numbers */
+        EdmPrimitiveTypeKind.Int16,
+        EdmPrimitiveTypeKind.Int32,
+        EdmPrimitiveTypeKind.Int64,
+        EdmPrimitiveTypeKind.Decimal,
+        EdmPrimitiveTypeKind.Double,
+        EdmPrimitiveTypeKind.Duration
+)
+
+private val ALLOWED_GEO_DATATYPES = ImmutableSet.of(
+        EdmPrimitiveTypeKind.GeographyPoint
+)
+
+private val ALLOWED_SCORE_DATATYPES = EdmPrimitiveTypeKind.values().toSet()
+
 enum class SortType {
     field,
     score,
-    geoDistance
+    geoDistance;
+
+    companion object {
+
+        @JvmStatic
+        fun getAllowedDatatypes(sortType: SortType): Set<EdmPrimitiveTypeKind> {
+            return when (sortType) {
+                field -> ALLOWED_FIELD_DATATYPES
+                geoDistance -> ALLOWED_GEO_DATATYPES
+                score -> ALLOWED_SCORE_DATATYPES
+            }
+        }
+    }
 }
+
+
