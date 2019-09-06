412f31e1c65e8f55ad58b16756ebbc9470ffb023
Piper Keyes <piper@openlattice.com>
2019-09-05 17:16:11 -0700

    changed to data class

--- api/src/main/kotlin/com/openlattice/data/DataExpiration.kt
+++ api/src/main/kotlin/com/openlattice/data/DataExpiration.kt
@@ -10,15 +10,13 @@ import java.util.concurrent.TimeUnit
  * Holds information about the expiration parameters of data within an entity set
  */
 
-class DataExpiration (
-        @JsonProperty( SerializationConstants.EXPIRATION ) timeToExpiration: Long,
-        @JsonProperty( SerializationConstants.FLAGS_FIELD ) expirationFlag: ExpirationType,
-        @JsonProperty( SerializationConstants.PROPERTY_TYPE_ID ) startDateProperty: Optional<UUID> = Optional.empty()
+data class DataExpiration(
+        @JsonProperty(SerializationConstants.EXPIRATION) val timeToExpiration: Long,
+        @JsonProperty(SerializationConstants.FLAGS_FIELD) val expirationFlag: ExpirationType,
+        @JsonProperty(SerializationConstants.PROPERTY_TYPE_ID) val startDateProperty: Optional<UUID> = Optional.empty()
 ) {
-    val timeToExpiration = timeToExpiration
-    val expirationFlag = expirationFlag
-    val startDateProperty = startDateProperty
+
     init {
-        if (expirationFlag == ExpirationType.DATE_PROPERTY) check(startDateProperty.isPresent) {"Must provide property type for expiration calculation"}
+        if (expirationFlag == ExpirationType.DATE_PROPERTY) check(startDateProperty.isPresent) { "Must provide property type for expiration calculation" }
     }
 }
