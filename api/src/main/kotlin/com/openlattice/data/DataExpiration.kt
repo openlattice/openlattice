95d4914befff238be8e1a1685efb722203cd3c45
Piper Keyes <piper@openlattice.com>
2019-09-10 11:06:39 -0700

    Added more documentation to make role of each parameter and their interaction clearer

--- api/src/main/kotlin/com/openlattice/data/DataExpiration.kt
+++ api/src/main/kotlin/com/openlattice/data/DataExpiration.kt
@@ -2,21 +2,30 @@ package com.openlattice.data
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.openlattice.client.serialization.SerializationConstants
-import com.openlattice.edm.set.ExpirationType
+import com.openlattice.edm.set.ExpirationBase
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 /**
- * Holds information about the expiration parameters of data within an entity set
+ * Holds information about the expiration parameters of data within an entity set.
+ *
+ * timeToExpiration holds the duration in ms that data will live before expiring.
+ *
+ * expirationBase is a flag for what will be used as a base date to calculate
+ * expiration date. It may be an entity's first write, last write, or a datetime
+ * property type within the entity. Thus, an entity's expiration datetime is calculated
+ * as expirationBase + timeToExpire
+ *
+ * If the expirationBase is a datetime property type, startDateProperty will hold the
+ * UUID of that property type. Otherwise, it will remain empty.
  */
 
 data class DataExpiration(
         @JsonProperty(SerializationConstants.EXPIRATION) val timeToExpiration: Long,
-        @JsonProperty(SerializationConstants.FLAGS_FIELD) val expirationFlag: ExpirationType,
+        @JsonProperty(SerializationConstants.FLAGS_FIELD) val expirationBase: ExpirationBase,
         @JsonProperty(SerializationConstants.PROPERTY_TYPE_ID) val startDateProperty: Optional<UUID> = Optional.empty()
 ) {
 
     init {
-        if (expirationFlag == ExpirationType.DATE_PROPERTY) check(startDateProperty.isPresent) { "Must provide property type for expiration calculation" }
+        if (expirationBase == ExpirationBase.DATE_PROPERTY) check(startDateProperty.isPresent) { "Must provide property type for expiration calculation" }
     }
 }
