4166707afebd994cb3d62e0509f09a85754848eb
Piper Keyes <piper@openlattice.com>
2019-09-20 10:58:54 -0700

    prevent endless re-clearing of already cleared entitites

--- indexer/src/main/kotlin/com/openlattice/indexing/BackgroundExpiredDataDeletionService.kt
+++ indexer/src/main/kotlin/com/openlattice/indexing/BackgroundExpiredDataDeletionService.kt
@@ -158,7 +158,7 @@ class BackgroundExpiredDataDeletionService(
                 ?: error("No partitions assigned to entity set ${entitySet.id}"))
                 .joinToString(prefix = "('", postfix = "')", separator = "', '") { it.toString() }
 
-        var entityKeyIds = getExpiredIds(entitySet.id, sqlParams, partitions).toSet()
+        var entityKeyIds = getExpiredIds(entitySet.id, sqlParams, partitions, entitySet.expiration.deleteType).toSet()
         if (entityKeyIds.isNotEmpty()) {  //if no data have expired let's not waste our time
             while (entityKeyIds.isNotEmpty()) { //loops until all expired entities have been removed from entity set
                 val chunkedEntityKeyIds = entityKeyIds.chunked(MAX_BATCH_SIZE)
@@ -190,7 +190,7 @@ class BackgroundExpiredDataDeletionService(
 
                     totalDeletedEntitiesCount += writeEvent.numUpdates
                     deletedEntityKeyIds.addAll(entityKeyIds)
-                    entityKeyIds = getExpiredIds(entitySet.id, sqlParams, partitions).toSet()
+                    entityKeyIds = getExpiredIds(entitySet.id, sqlParams, partitions, entitySet.expiration.deleteType).toSet()
                 }
             }
 
@@ -241,11 +241,12 @@ class BackgroundExpiredDataDeletionService(
         return Triple(comparisonField, expirationField, expirationFieldSQLType)
     }
 
-    private fun getExpiredIds(entitySetId: UUID, sqlParams: Triple<String, Any, Int>, partitions: String): PostgresIterable<UUID> {
+    private fun getExpiredIds(entitySetId: UUID, sqlParams: Triple<String, Any, Int>, partitions: String, deleteType: DeleteType): PostgresIterable<UUID> {
+        val getExpiredIdsQuery = getExpiredIdsQuery(entitySetId, sqlParams.first, partitions, deleteType)
         return PostgresIterable(
                 Supplier {
                     val connection = hds.connection
-                    val stmt = connection.prepareStatement(getExpiredIdsQuery(entitySetId, sqlParams.first, partitions))
+                    val stmt = connection.prepareStatement(getExpiredIdsQuery(entitySetId, sqlParams.first, partitions, deleteType))
                     stmt.setObject(1, sqlParams.second, sqlParams.third)
                     StatementHolder(connection, stmt, stmt.executeQuery())
                 },
@@ -286,12 +287,17 @@ class BackgroundExpiredDataDeletionService(
         return elasticsearchApi.deleteEntityDataBulk(entitySetId, entityTypeId, expiredEntityKeyIds)
     }
 
-    private fun getExpiredIdsQuery(entitySetId: UUID, comparisonField: String, partitions: String): String {
+    private fun getExpiredIdsQuery(entitySetId: UUID, comparisonField: String, partitions: String, deleteType: DeleteType): String {
+        var ignoredClearedEntitiesClause = ""
+        if (deleteType == DeleteType.Soft) {
+            ignoredClearedEntitiesClause = "AND ${VERSION.name} >= 0 "
+        }
         return "SELECT ${ID.name} FROM ${DATA.name} " +
                 "WHERE ${ENTITY_SET_ID.name} = '$entitySetId' " +
                 "AND ${PARTITION.name} IN $partitions " +
                 "AND $comparisonField < ? " +
                 "AND ${PROPERTY_TYPE_ID.name} != '00000000-0000-0001-0000-000000000014' " +
+                ignoredClearedEntitiesClause + // this clause prevents re-clearing of cleared entities
                 "LIMIT $FETCH_SIZE"
     }
 
