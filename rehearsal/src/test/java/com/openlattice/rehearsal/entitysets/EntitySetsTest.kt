a2ca5a9a359b44ec7f0a194ccc4af8411e608547
Piper Keyes <piper@openlattice.com>
2019-09-23 15:37:58 -0700

    added test for checking if entities have expired

--- rehearsal/src/test/java/com/openlattice/rehearsal/entitysets/EntitySetsTest.kt
+++ rehearsal/src/test/java/com/openlattice/rehearsal/entitysets/EntitySetsTest.kt
@@ -1,10 +1,14 @@
 package com.openlattice.rehearsal.entitysets
 
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.google.common.cache.LoadingCache
 import com.google.common.collect.ImmutableList
 import com.openlattice.data.DataExpiration
 import com.openlattice.data.DeleteType
 import com.openlattice.edm.requests.MetadataUpdate
 import com.openlattice.edm.set.ExpirationBase
+import com.openlattice.edm.type.EntityType
 import com.openlattice.graph.query.GraphQueryState
 import com.openlattice.mapstores.TestDataFactory
 import com.openlattice.rehearsal.assertException
@@ -12,19 +16,36 @@ import com.openlattice.rehearsal.authentication.MultipleAuthenticatedUsersBase
 import com.openlattice.rehearsal.edm.EdmTestConstants
 import com.openlattice.rehearsal.organization.OrganizationControllerCallHelper
 import com.openlattice.rehearsal.organization.OrganizationsControllerTest
+import org.apache.commons.lang.RandomStringUtils
+import org.apache.olingo.commons.api.edm.FullQualifiedName
 import org.junit.Assert
 import org.junit.BeforeClass
 import org.junit.Test
 import java.lang.reflect.UndeclaredThrowableException
+import java.time.OffsetDateTime
 import java.util.*
 
 class EntitySetsTest : MultipleAuthenticatedUsersBase() {
 
     companion object {
+        val fqnCache: LoadingCache<UUID, FullQualifiedName> = CacheBuilder.newBuilder()
+                .build(
+                        object : CacheLoader<UUID, FullQualifiedName>() {
+                            override fun load(key: UUID?): FullQualifiedName {
+                                return edmApi.getPropertyType(key!!).type
+                            }
+
+                        }
+                )
+
+        lateinit var personEt: EntityType
+
         @JvmStatic
         @BeforeClass
         fun init() {
             loginAs("admin")
+
+            personEt = EdmTestConstants.personEt
         }
     }
 
@@ -122,4 +143,28 @@ class EntitySetsTest : MultipleAuthenticatedUsersBase() {
         val es3 = entitySetsApi.getEntitySet(es.id)
         Assert.assertNull(es3.expiration)
     }
+
+    @Test
+    fun testGetExpiringEntities() {
+        //create data to expire
+        val es = createEntitySet(personEt)
+        val entries = (1..10)
+                .map { mapOf(EdmTestConstants.personGivenNameId to setOf(RandomStringUtils.randomAscii(5))) }
+        val newEntityIds = dataApi.createEntities(es.id, entries)
+
+        //set expiration policy
+        val tTL = 10L
+        val expirationPolicy = DataExpiration(tTL, ExpirationBase.FIRST_WRITE, DeleteType.Hard)
+        val update = MetadataUpdate(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.of(expirationPolicy))
+        entitySetsApi.updateEntitySetMetadata(es.id, update)
+        val es2 = entitySetsApi.getEntitySet(es.id)
+
+        //check expiring entities
+        val expiringIds = entitySetsApi.getExpiringEntitiesFromEntitySet(es.id, OffsetDateTime.now().toString())
+        Assert.assertEquals(10, expiringIds.size)
+    }
+
 }
