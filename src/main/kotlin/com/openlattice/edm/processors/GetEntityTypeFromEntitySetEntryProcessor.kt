75bebd934deb2b5ff036ddb5fb3eeeb8faeae31d
Drew <drew@openlattice.com>
2019-09-25 10:37:50 -0400

    updated EPs to have nullable values and non-null Entry

--- src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
@@ -6,8 +6,8 @@ import com.openlattice.edm.EntitySet
 import java.util.*
 
 class GetEntityTypeFromEntitySetEntryProcessor : AbstractRhizomeEntryProcessor<UUID, EntitySet, Optional<UUID>>(), ReadOnly {
-    override fun process(entry: MutableMap.MutableEntry<UUID, EntitySet>?): Optional<UUID> {
-        val es = entry?.value ?: return Optional.empty()
+    override fun process(entry: MutableMap.MutableEntry<UUID, EntitySet?>): Optional<UUID> {
+        val es = entry.value ?: return Optional.empty()
         return Optional.of( es.entityTypeId )
     }
 }
--- src/main/kotlin/com/openlattice/edm/processors/GetPropertiesFromEntityTypeEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/edm/processors/GetPropertiesFromEntityTypeEntryProcessor.kt
@@ -6,8 +6,8 @@ import com.openlattice.edm.type.EntityType
 import java.util.*
 
 class GetPropertiesFromEntityTypeEntryProcessor: AbstractRhizomeEntryProcessor<UUID, EntityType, Optional<Set<UUID>>>(), ReadOnly {
-    override fun process(entry: MutableMap.MutableEntry<UUID, EntityType>?): Optional<Set<UUID>>? {
-        val et = entry?.value ?: return Optional.empty()
+    override fun process(entry: MutableMap.MutableEntry<UUID, EntityType?>): Optional<Set<UUID>>? {
+        val et = entry.value ?: return Optional.empty()
         return Optional.of( et.properties )
     }
 }
