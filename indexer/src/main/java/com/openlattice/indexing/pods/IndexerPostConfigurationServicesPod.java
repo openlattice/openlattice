41ec40c5548f70985d733ebd38e3ee34ea5fcb9d
Piper Keyes <piper@openlattice.com>
2019-09-26 15:12:06 -0700

    unused code again

--- indexer/src/main/java/com/openlattice/indexing/pods/IndexerPostConfigurationServicesPod.java
+++ indexer/src/main/java/com/openlattice/indexing/pods/IndexerPostConfigurationServicesPod.java
@@ -160,7 +160,6 @@ public class IndexerPostConfigurationServicesPod {
         return new BackgroundExpiredDataDeletionService(
                 hazelcastInstance,
                 indexerConfiguration,
-                elasticsearchApi,
                 auditingManager,
                 dataGraphService,
                 edm);
