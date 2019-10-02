df5f019da113c5a72bdf5eb64f8111a7d8c8b155
Drew <drew@openlattice.com>
2019-10-02 14:03:06 -0400

    Add EntryProcessors for use in Assembler

---
+++ src/main/kotlin/com/openlattice/edm/processors/IsAssemblyInitializedEntryProcessor.kt
@@ -0,0 +1,13 @@
+package com.openlattice.edm.processors
+
+import com.hazelcast.core.ReadOnly
+import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor
+import com.openlattice.assembler.OrganizationAssembly
+import java.util.*
+
+class IsAssemblyInitializedEntryProcessor : AbstractRhizomeEntryProcessor<UUID, OrganizationAssembly, Boolean>(), ReadOnly {
+    override fun process(entry: MutableMap.MutableEntry<UUID, OrganizationAssembly?>): Boolean {
+        val orgAssembly = entry.value ?: return false
+        return orgAssembly.initialized
+    }
+}
---
+++ src/main/kotlin/com/openlattice/hazelcast/serializers/IsAssemblyInitializedEntryProcessorStreamSerializer.kt
@@ -0,0 +1,15 @@
+package com.openlattice.hazelcast.serializers
+
+import com.hazelcast.nio.ObjectDataInput
+import com.openlattice.edm.processors.IsAssemblyInitializedEntryProcessor
+
+@Component
+class IsAssemblyInitializedEntryProcessorStreamSerializer : NoOpSelfRegisteringStreamSerializer<IsAssemblyInitializedEntryProcessor>() {
+    override fun getClazz(): Class<out IsAssemblyInitializedEntryProcessor> {
+        return IsAssemblyInitializedEntryProcessor::class.java
+    }
+
+    override fun read(`in`: ObjectDataInput?): IsAssemblyInitializedEntryProcessor {
+        return IsAssemblyInitializedEntryProcessor()
+    }
+}
