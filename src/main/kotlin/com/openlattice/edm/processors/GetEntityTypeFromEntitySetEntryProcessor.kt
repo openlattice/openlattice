814d7f0a8020294190f134c60c056dbfbfc1856c
Drew <drew@openlattice.com>
2019-10-04 18:24:09 -0400

    Dont use optionals I guess. I hate them anyway sometimes. Who knew?

--- src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
@@ -1,14 +1,12 @@
 package com.openlattice.edm.processors
 
-import com.hazelcast.core.ReadOnly
-import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor
 import com.openlattice.edm.EntitySet
 import com.openlattice.rhizome.hazelcast.entryprocessors.AbstractReadOnlyRhizomeEntryProcessor
 import java.util.*
 
-class GetEntityTypeFromEntitySetEntryProcessor : AbstractReadOnlyRhizomeEntryProcessor<UUID, EntitySet, Optional<UUID>>() {
-    override fun process(entry: MutableMap.MutableEntry<UUID, EntitySet?>): Optional<UUID> {
-        val es = entry.value ?: return Optional.empty()
-        return Optional.of( es.entityTypeId )
+class GetEntityTypeFromEntitySetEntryProcessor : AbstractReadOnlyRhizomeEntryProcessor<UUID, EntitySet, UUID?>() {
+    override fun process(entry: MutableMap.MutableEntry<UUID, EntitySet?>): UUID? {
+        val es = entry.value ?: return null
+        return es.entityTypeId
     }
 }
