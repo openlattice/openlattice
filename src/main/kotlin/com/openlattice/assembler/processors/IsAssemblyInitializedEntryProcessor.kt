f891598567bfd5c7778889c9f647bc0cdba388cd
you-leee <julia@openlattice.com>
2019-10-04 11:05:53 -0700

    applyOnBackup=false on readonly entryProcessor

--- src/main/kotlin/com/openlattice/assembler/processors/IsAssemblyInitializedEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/assembler/processors/IsAssemblyInitializedEntryProcessor.kt
@@ -5,7 +5,7 @@ import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcesso
 import com.openlattice.assembler.OrganizationAssembly
 import java.util.*
 
-class IsAssemblyInitializedEntryProcessor : AbstractRhizomeEntryProcessor<UUID, OrganizationAssembly, Boolean>(), ReadOnly {
+class IsAssemblyInitializedEntryProcessor : AbstractRhizomeEntryProcessor<UUID, OrganizationAssembly, Boolean>(false), ReadOnly {
     override fun process(entry: MutableMap.MutableEntry<UUID, OrganizationAssembly?>): Boolean {
         val orgAssembly = entry.value ?: return false
         return orgAssembly.initialized
