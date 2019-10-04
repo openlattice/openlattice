a6354d20bed4a08841cddc4b3a699bf23534d115
you-leee <julia@openlattice.com>
2019-10-04 14:47:50 -0700

    LATTICE-1913 make IsAssemblyInitializedEntryProcessor readonly

--- src/main/kotlin/com/openlattice/assembler/processors/IsAssemblyInitializedEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/assembler/processors/IsAssemblyInitializedEntryProcessor.kt
@@ -1,11 +1,12 @@
-package com.openlattice.edm.processors
+package com.openlattice.assembler.processors
 
 import com.hazelcast.core.ReadOnly
 import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor
 import com.openlattice.assembler.OrganizationAssembly
+import com.openlattice.rhizome.hazelcast.entryprocessors.AbstractReadOnlyRhizomeEntryProcessor
 import java.util.*
 
-class IsAssemblyInitializedEntryProcessor : AbstractRhizomeEntryProcessor<UUID, OrganizationAssembly, Boolean>(), ReadOnly {
+class IsAssemblyInitializedEntryProcessor : AbstractReadOnlyRhizomeEntryProcessor<UUID, OrganizationAssembly, Boolean>() {
     override fun process(entry: MutableMap.MutableEntry<UUID, OrganizationAssembly?>): Boolean {
         val orgAssembly = entry.value ?: return false
         return orgAssembly.initialized
