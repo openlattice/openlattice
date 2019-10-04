6695056b3d0881f363920f9b96f5db796339ed10
Drew <drew@openlattice.com>
2019-10-04 14:17:06 -0400

    update EPs that are readonly abstractEPs to use the direct type

--- src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/edm/processors/GetEntityTypeFromEntitySetEntryProcessor.kt
@@ -3,9 +3,10 @@ package com.openlattice.edm.processors
 import com.hazelcast.core.ReadOnly
 import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor
 import com.openlattice.edm.EntitySet
+import com.openlattice.rhizome.hazelcast.entryprocessors.AbstractReadOnlyRhizomeEntryProcessor
 import java.util.*
 
-class GetEntityTypeFromEntitySetEntryProcessor : AbstractRhizomeEntryProcessor<UUID, EntitySet, Optional<UUID>>(), ReadOnly {
+class GetEntityTypeFromEntitySetEntryProcessor : AbstractReadOnlyRhizomeEntryProcessor<UUID, EntitySet, Optional<UUID>>() {
     override fun process(entry: MutableMap.MutableEntry<UUID, EntitySet?>): Optional<UUID> {
         val es = entry.value ?: return Optional.empty()
         return Optional.of( es.entityTypeId )
--- src/main/kotlin/com/openlattice/edm/processors/GetPropertiesFromEntityTypeEntryProcessor.kt
+++ src/main/kotlin/com/openlattice/edm/processors/GetPropertiesFromEntityTypeEntryProcessor.kt
@@ -3,9 +3,10 @@ package com.openlattice.edm.processors
 import com.hazelcast.core.ReadOnly
 import com.kryptnostic.rhizome.hazelcast.processors.AbstractRhizomeEntryProcessor
 import com.openlattice.edm.type.EntityType
+import com.openlattice.rhizome.hazelcast.entryprocessors.AbstractReadOnlyRhizomeEntryProcessor
 import java.util.*
 
-class GetPropertiesFromEntityTypeEntryProcessor: AbstractRhizomeEntryProcessor<UUID, EntityType, Optional<Set<UUID>>>(), ReadOnly {
+class GetPropertiesFromEntityTypeEntryProcessor: AbstractReadOnlyRhizomeEntryProcessor<UUID, EntityType, Optional<Set<UUID>>>() {
     override fun process(entry: MutableMap.MutableEntry<UUID, EntityType?>): Optional<Set<UUID>>? {
         val et = entry.value ?: return Optional.empty()
         return Optional.of( et.properties )
