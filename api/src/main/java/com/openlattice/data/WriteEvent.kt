0227667e906f4efbfdd54c6ea5f7b2958d5f2076
you-leee <julia@openlattice.com>
2019-10-10 12:14:09 -0700

    LATTICE-2086 add doc

--- api/src/main/java/com/openlattice/data/WriteEvent.kt
+++ api/src/main/java/com/openlattice/data/WriteEvent.kt
@@ -2,6 +2,13 @@ package com.openlattice.data
 
 import kotlin.math.abs
 
+/**
+ * Represents an write event used for auditing.
+ * @param version The timestamp when the event occurred.
+ * Note: The timestamp should always be the time, when the event occurred, so it will show up in a time range query.
+ * For this reason the absolute value will be used.
+ * @param numUpdates
+ */
 data class WriteEvent(var version: Long, val numUpdates: Int) {
     init {
         version = abs(version)
