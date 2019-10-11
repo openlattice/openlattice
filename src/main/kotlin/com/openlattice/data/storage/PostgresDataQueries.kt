f157172c966dc4d39427f6f272f5a9fd7678bd1f
Katherine Bernstein <katherine@openlattice.com>
2019-10-11 10:44:27 -0700

    missed some

--- src/main/kotlin/com/openlattice/data/storage/PostgresDataQueries.kt
+++ src/main/kotlin/com/openlattice/data/storage/PostgresDataQueries.kt
@@ -644,7 +644,7 @@ fun upsertPropertyValueSql(propertyType: PropertyType): String {
             "${LAST_WRITE.name} = GREATEST(${DATA.name}.${LAST_WRITE.name},EXCLUDED.${LAST_WRITE.name}), " +
             "${PARTITIONS_VERSION.name} = EXCLUDED.${PARTITIONS_VERSION.name}, " +
             "${VERSION.name} = CASE " +
-                "WHEN abs(${DATA.name}.${VERSION.name}) < EXCLUDED.${VERSION.name} " +
+                "WHEN abs(${DATA.name}.${VERSION.name}) <= EXCLUDED.${VERSION.name} " +
                 "THEN EXCLUDED.${VERSION.name} " +
                 "ELSE ${DATA.name}.${VERSION.name} " +
             "END"
@@ -687,7 +687,7 @@ fun upsertPropertyValueLinkingRowSql(propertyType: PropertyType ): String {
             "${LAST_WRITE.name} = GREATEST(${DATA.name}.${LAST_WRITE.name},EXCLUDED.${LAST_WRITE.name}), " +
             "${PARTITIONS_VERSION.name} = EXCLUDED.${PARTITIONS_VERSION.name}, " +
             "${VERSION.name} = CASE " +
-                "WHEN abs(${DATA.name}.${VERSION.name}) < EXCLUDED.${VERSION.name} " +
+                "WHEN abs(${DATA.name}.${VERSION.name}) <= EXCLUDED.${VERSION.name} " +
                 "THEN EXCLUDED.${VERSION.name} " +
                 "ELSE ${DATA.name}.${VERSION.name} " +
             "END"
@@ -726,7 +726,7 @@ fun createOrUpdateLinkFromEntity(): String {
                 "${LAST_WRITE.name} = GREATEST(${DATA.name}.${LAST_WRITE.name},EXCLUDED.${LAST_WRITE.name}), " +
                 "${PARTITIONS_VERSION.name} = EXCLUDED.${PARTITIONS_VERSION.name}, " +
                 "${VERSION.name} = CASE " +
-                    "WHEN abs(${DATA.name}.${VERSION.name}) < EXCLUDED.${VERSION.name} " +
+                    "WHEN abs(${DATA.name}.${VERSION.name}) <= EXCLUDED.${VERSION.name} " +
                     "THEN EXCLUDED.${VERSION.name} " +
                     "ELSE ${DATA.name}.${VERSION.name} " +
                 "END"
