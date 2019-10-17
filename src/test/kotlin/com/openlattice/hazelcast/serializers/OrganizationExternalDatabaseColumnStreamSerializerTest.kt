6056ebffba09a43094f13fc2ecdc4e6dfbe51b09
Piper Keyes <piper@openlattice.com>
2019-10-16 17:05:19 -0700

    added stream serializer tests

--- src/test/kotlin/com/openlattice/hazelcast/serializers/OrganizationExternalDatabaseColumnStreamSerializerTest.kt
+++ src/test/kotlin/com/openlattice/hazelcast/serializers/OrganizationExternalDatabaseColumnStreamSerializerTest.kt
@@ -1,4 +1,17 @@
 package com.openlattice.hazelcast.serializers
 
-class OrganizationExternalDatabaseColumnStreamSerializerTest {
+import com.kryptnostic.rhizome.hazelcast.serializers.AbstractStreamSerializerTest
+import com.openlattice.mapstores.TestDataFactory
+import com.openlattice.organization.OrganizationExternalDatabaseColumn
+
+class OrganizationExternalDatabaseColumnStreamSerializerTest :
+        AbstractStreamSerializerTest<OrganizationExternalDatabaseColumnStreamSerializer, OrganizationExternalDatabaseColumn>() {
+
+    override fun createSerializer(): OrganizationExternalDatabaseColumnStreamSerializer {
+        return OrganizationExternalDatabaseColumnStreamSerializer()
+    }
+
+    override fun createInput(): OrganizationExternalDatabaseColumn {
+        return TestDataFactory.organizationExternalDatabaseColumn()
+    }
 }
