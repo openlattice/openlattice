9d4fc077d96c93cd1d726d664b515ad489e8ff41
you-leee <julia@openlattice.com>
2019-10-22 18:00:51 -0700

    LATTICE-2225 remove unused entitySetId from deleteEntityDataBulk

--- src/main/java/com/openlattice/datastore/services/DatastoreConductorElasticsearchApi.java
+++ src/main/java/com/openlattice/datastore/services/DatastoreConductorElasticsearchApi.java
@@ -307,10 +307,10 @@ public class DatastoreConductorElasticsearchApi implements ConductorElasticsearc
         }
     }
 
-    @Override public boolean deleteEntityDataBulk( UUID entitySetId, UUID entityTypeId, Set<UUID> entityKeyIds ) {
+    @Override public boolean deleteEntityDataBulk( UUID entityTypeId, Set<UUID> entityKeyIds ) {
         try {
             return executor.submit( ConductorElasticsearchCall
-                    .wrap( ElasticsearchLambdas.deleteEntityDataBulk( entitySetId, entityTypeId, entityKeyIds ) ) )
+                    .wrap( ElasticsearchLambdas.deleteEntityDataBulk( entityTypeId, entityKeyIds ) ) )
                     .get();
         } catch ( InterruptedException | ExecutionException e ) {
             logger.debug( "unable to delete entity data from elasticsearch" );
