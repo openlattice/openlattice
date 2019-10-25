369d6583a4539b2b255571164a9d64865aec4580
Piper Keyes <piper@openlattice.com>
2019-10-24 17:19:07 -0700

    added index for id

--- src/main/kotlin/com/openlattice/organizations/mapstores/OrganizationExternalDatabaseColumnMapstore.kt
+++ src/main/kotlin/com/openlattice/organizations/mapstores/OrganizationExternalDatabaseColumnMapstore.kt
@@ -14,6 +14,7 @@ import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.util.*
 
+const val ID_INDEX = "key"
 const val TABLE_ID_INDEX = "tableId"
 const val ORGANIZATION_ID_INDEX = "organizationId"
 
@@ -62,6 +63,7 @@ open class OrganizationExternalDatabaseColumnMapstore(
 
     override fun getMapConfig(): MapConfig {
         return super.getMapConfig()
+                .addMapIndexConfig(MapIndexConfig(ID_INDEX, false))
                 .addMapIndexConfig(MapIndexConfig(TABLE_ID_INDEX, false))
                 .addMapIndexConfig(MapIndexConfig(ORGANIZATION_ID_INDEX, false))
                 .setInMemoryFormat( InMemoryFormat.OBJECT )
