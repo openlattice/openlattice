919e1ddcd6acf4c4896d3a94f5c75bf2b587da34
Piper Keyes <piper@openlattice.com>
2019-11-01 10:15:07 -0700

    added connetion method and auth method to config

--- src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
+++ src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
@@ -4,6 +4,8 @@ import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration
 
 @ReloadableConfiguration(uri = "pg_hba_config.yaml")
 data class OrganizationExternalDatabaseConfiguration(
-        val path: String
+        val path: String,
+        val connectionMethod: String,
+        val authMethod: String
 ) {
 }
