3287ae9c3f241699af08bb109184b1b66e4681bd
Piper Keyes <piper@openlattice.com>
2019-11-01 12:47:18 -0700

    removed connection type as a hard coded field

--- src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
+++ src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
@@ -5,7 +5,6 @@ import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration
 @ReloadableConfiguration(uri = "pg_hba_config.yaml")
 data class OrganizationExternalDatabaseConfiguration(
         val path: String,
-        val connectionMethod: String,
         val authMethod: String
 ) {
 }
