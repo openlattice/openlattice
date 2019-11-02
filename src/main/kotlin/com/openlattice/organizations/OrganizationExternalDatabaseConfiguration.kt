45ac41b1562c734549e67d6e42e89a78ecd5cc4f
Piper Keyes <piper@openlattice.com>
2019-11-01 17:33:06 -0700

    split path into path and file name for copying newly made file

--- src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
+++ src/main/kotlin/com/openlattice/organizations/OrganizationExternalDatabaseConfiguration.kt
@@ -5,6 +5,7 @@ import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration
 @ReloadableConfiguration(uri = "pg_hba_config.yaml")
 data class OrganizationExternalDatabaseConfiguration(
         val path: String,
+        val fileName: String,
         val authMethod: String
 ) {
 }
