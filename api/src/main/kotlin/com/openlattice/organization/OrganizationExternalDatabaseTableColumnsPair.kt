2891acdb6a04ea6e031a03c4d7cdc3be3cc2a44e
Piper Keyes <piper@openlattice.com>
2019-11-04 16:54:00 -0800

    data class for holding an org table object and its column objects

--- api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseTableColumnsPair.kt
+++ api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseTableColumnsPair.kt
@@ -1,4 +1,8 @@
 package com.openlattice.organization
 
-class OrganizationExternalDatabaseTableColumnsPair {
-}
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.openlattice.client.serialization.SerializationConstants
+
+data class OrganizationExternalDatabaseTableColumnsPair(
+        @JsonProperty(SerializationConstants.TABLE) val table: OrganizationExternalDatabaseTable,
+        @JsonProperty(SerializationConstants.COLUMNS) val columns: Set<OrganizationExternalDatabaseColumn>)
