863d7a42c24177096c394ca2e0ee18cd5e498537
Katherine Bernstein <katherine@openlattice.com>
2019-11-06 16:51:12 -0700

    kotlify pt 2

---
+++ api/src/main/kotlin/com/openlattice/apps/UserAppConfig.kt
@@ -0,0 +1,11 @@
+package com.openlattice.apps
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.openlattice.client.serialization.SerializationConstants
+import java.util.*
+
+data class UserAppConfig(
+        @JsonProperty(SerializationConstants.ORGANIZATION_ID) val organizationId: UUID,
+        @JsonProperty(SerializationConstants.ENTITY_SET_COLLECTION_ID) val entitySetCollectionId: UUID,
+        @JsonProperty(SerializationConstants.ROLES) val roles: Set<UUID>
+)
