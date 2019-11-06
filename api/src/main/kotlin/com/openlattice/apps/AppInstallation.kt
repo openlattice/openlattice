589a67d73e1a002c5a38f4ca25b9ebd726a3c12d
Katherine Bernstein <katherine@openlattice.com>
2019-11-06 15:00:07 -0700

    kotlify #1

---
+++ api/src/main/kotlin/com/openlattice/apps/AppInstallation.kt
@@ -0,0 +1,33 @@
+package com.openlattice.apps
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.google.common.base.Preconditions
+import com.openlattice.client.serialization.SerializationConstants
+import java.util.*
+
+data class AppInstallation(
+        @JsonProperty(SerializationConstants.ENTITY_SET_COLLECTION_ID) val entitySetCollectionId: UUID?,
+        @JsonProperty(SerializationConstants.PREFIX) val prefix: String?,
+        @JsonProperty(SerializationConstants.TEMPLATE) val template: Map<UUID, UUID>?,
+        @JsonProperty(SerializationConstants.SETTINGS) val settings: Map<String, Any>?) {
+
+    init {
+
+        Preconditions.checkArgument(entitySetCollectionId != null || template != null,
+                "entitySetCollectionId and template cannot both be empty for AppInstallation")
+
+        if (entitySetCollectionId != null) {
+            Preconditions.checkArgument(prefix != null && template != null,
+                    "If AppInstallation specifies entitySetCollectionId, prefix and template must be empty.")
+        }
+
+        if (prefix != null) {
+            Preconditions
+                    .checkArgument(template != null, "AppInstallation cannot specify prefix but not template.")
+        }
+        if (template != null) {
+            Preconditions
+                    .checkArgument(prefix != null, "AppInstallation cannot specify template but not prefix.")
+        }
+    }
+}
