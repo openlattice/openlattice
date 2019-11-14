0fdd2c3dbf197655b80e897b884afbb8bfef4f33
jokedurnez <joke.durnez@gmail.com>
2019-11-13 18:10:12 -0800

    add test for entitytypepropertymetadata

--- rehearsal/src/test/java/com/openlattice/rehearsal/edm/EdmTest.kt
+++ rehearsal/src/test/java/com/openlattice/rehearsal/edm/EdmTest.kt
@@ -24,6 +24,7 @@ package com.openlattice.rehearsal.edm
 import com.google.common.collect.ImmutableList
 import com.openlattice.data.requests.EntitySetSelection
 import com.openlattice.data.requests.FileType
+import com.openlattice.edm.requests.MetadataUpdate
 import com.openlattice.edm.type.AssociationType
 import com.openlattice.mapstores.TestDataFactory
 import com.openlattice.rehearsal.assertException
@@ -182,5 +183,37 @@ class EdmTest : MultipleAuthenticatedUsersBase() {
                 { edmApi.getEntityType(et.id) },
                 "Entity type of id ${et.id} does not exists."
         )
+    }
+
+    @Test
+    fun testEntityTypePropertyTypeMetadata() {
+        val pt1 = createPropertyType()
+        val et = createEntityType(pt1.id)
+        val update =  MetadataUpdate(
+                Optional.of("New Title !"),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty()
+        )
+        edmApi.updateEntityTypePropertyMetadata( et.id , pt1.id, update )
+        var metadata = edmApi.getEntityTypePropertyMetadata(et.id, pt1.id);
+        Assert.assertEquals(
+                "New Title !",
+                metadata.title
+        )
+        Assert.assertEquals(
+                pt1.description,
+                metadata.description
+        )
+
     }
 }
