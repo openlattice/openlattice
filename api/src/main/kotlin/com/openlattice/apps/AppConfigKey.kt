e2bc531fce9cfae7c2f572dc25ab11bcbf216ec6
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-11-16 23:03:23 -0800

    Compile

--- api/src/main/kotlin/com/openlattice/apps/AppConfigKey.kt
+++ api/src/main/kotlin/com/openlattice/apps/AppConfigKey.kt
@@ -1,9 +1,15 @@
 package com.openlattice.apps
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.openlattice.client.serialization.SerializationConstants
 import java.util.*
 
 /**
  *
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
-data class AppConfigKey( val organizationId:UUID, val appId:UUID ,val appTypeId:UUID)
+data class AppConfigKey(
+        @JsonProperty(SerializationConstants.APP_ID) val appId: UUID,
+        @JsonProperty( SerializationConstants.ORGANIZATION_ID )  val organizationId: UUID,
+        @JsonProperty( SerializationConstants.APP_TYPE_ID ) val appTypeId: UUID
+)
