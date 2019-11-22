e87d11162feacbf01fd5fada7c764a63ee66ac6f
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-11-21 17:06:04 -0800

    fix compile

--- api/src/main/kotlin/com/openlattice/users/Auth0UserUtils.kt
+++ api/src/main/kotlin/com/openlattice/users/Auth0UserUtils.kt
@@ -3,10 +3,6 @@ package com.openlattice.users
 import com.auth0.json.mgmt.users.User
 import com.openlattice.authorization.Principal
 import com.openlattice.authorization.PrincipalType
-import com.openlattice.authorization.SystemRole
-import com.openlattice.organizations.roles.SecurePrincipalsManager
-import jodd.mail.Email
-import jodd.mail.EmailAddress
 import org.apache.commons.validator.routines.EmailValidator
 
 /**
@@ -17,26 +13,26 @@ import org.apache.commons.validator.routines.EmailValidator
 /**
  * @return The roles of user
  */
-fun getRoles( user: User) : Set<String> {
-    return ( ( user.appMetadata?.getOrDefault( "roles", listOf<String>()) ?: listOf<String>() )as List<String>).toSet()
+fun getRoles(user: User): Set<String> {
+    return ((user.appMetadata?.getOrDefault("roles", listOf<String>()) ?: listOf<String>()) as List<String>).toSet()
 }
 
 fun getPrincipal(user: User): Principal {
     return Principal(PrincipalType.USER, user.id)
 }
 
-fun getEmailDomain( user: User ) : String {
+fun getEmailDomain(user: User): String {
     return getEmailDomain(user.email)
 }
 
 fun getEmailDomain(email: String): String {
-    require( isValidEmail(email) ) {
+    require(isValidEmail(email)) {
         "Email $email is not valid e-mail address."
     }
     return email.substring(email.indexOf("@"))
 }
 
-fun isValidEmail(email: String ) : Boolean {
+fun isValidEmail(email: String): Boolean {
     val atIndex = email.indexOf("@")
     return (atIndex != -1) && (atIndex != (email.length - 1)) && EmailValidator.getInstance().isValid(email)
 }
