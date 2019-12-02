a99505b6922fd4b2332c3d694936d6162261a2c7
Piper Keyes <piper@openlattice.com>
2019-12-02 14:14:49 -0800

    flight entity set fqns

---
+++ src/main/kotlin/com/openlattice/shuttle/FlightConfiguration.kt
@@ -0,0 +1,10 @@
+package com.openlattice.shuttle
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration
+
+@ReloadableConfiguration( uri = "flight.yaml")
+data class FlightConfiguration(
+        @JsonProperty("entity-type") val entityTypeFqn: String,
+        @JsonProperty("fqns") val fqns: Map<FlightProperty, String>
+)
---
+++ src/main/kotlin/com/openlattice/shuttle/FlightProperty.kt
@@ -0,0 +1,9 @@
+package com.openlattice.shuttle
+
+enum class FlightProperty {
+    DEFINITION,
+    ARGS,
+    CONTACT,
+    SQL,
+    PKEY
+}
