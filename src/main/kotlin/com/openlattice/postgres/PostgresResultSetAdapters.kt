7963d433026e5dee08ae94bd904bd1f6c44eceeb
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-12-04 14:52:08 -0800

    Don't read in loop

--- src/main/kotlin/com/openlattice/postgres/PostgresResultSetAdapters.kt
+++ src/main/kotlin/com/openlattice/postgres/PostgresResultSetAdapters.kt
@@ -166,6 +166,7 @@ fun readJsonDataColumnsWithId(
     detailedEntity.forEach { (propertyTypeId, details) ->
         // only select properties which are authorized
         if (propertyTypes.keys.contains(propertyTypeId)) {
+            val lastWrite = lastWriteTyped(rs)
             details.forEach { entityDetail ->
                 val originId = UUID.fromString(entityDetail[PostgresColumn.ID_VALUE.name] as String)
                 val propertyValue = entityDetail.getValue(VALUE)
@@ -177,7 +178,6 @@ fun readJsonDataColumnsWithId(
                 }
 
                 if (metadataOptions.contains(MetadataOption.LAST_WRITE)) {
-                    val lastWrite = lastWriteTyped(rs)
                     entities.getValue(originId)[LAST_WRITE_ID.id] = mutableSetOf<Any>(lastWrite)
                 }
             }
