d3d48720928ce2da3b3f7b48717bccc39dc33a46
Katherine Bernstein <katherine@openlattice.com>
2019-12-10 16:29:36 -0800

    kotlify AppTypeSetting

---
+++ api/src/main/kotlin/com/openlattice/apps/AppTypeSetting.kt
@@ -0,0 +1,31 @@
+package com.openlattice.apps
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.openlattice.authorization.AclKey
+import com.openlattice.client.serialization.SerializationConstants
+import java.util.*
+
+data class AppTypeSetting(
+        @JsonProperty(SerializationConstants.ID_FIELD) val id: UUID,
+        @JsonProperty(SerializationConstants.ENTITY_SET_COLLECTION_ID) val entitySetCollectionId: UUID,
+        @JsonProperty(SerializationConstants.ROLES) val roles: MutableMap<UUID, AclKey>,
+        @JsonProperty(SerializationConstants.SETTINGS) val settings: MutableMap<String, Any>
+) {
+
+    fun updateSettings(settingsUpdates: Map<String, Object>) {
+        settings.putAll(settingsUpdates)
+    }
+
+    fun removeSettings(settingsKeys: Set<String>) {
+        settingsKeys.forEach { settings.remove(it) }
+    }
+
+    fun addRole( roleId: UUID, roleAclKey: AclKey ) {
+        roles[roleId] = roleAclKey
+    }
+
+    fun removeRole(roleId: UUID) {
+        roles.remove(roleId)
+    }
+
+}
