df81d2195ea57fe9fcc3dd9f7cc6e020ee1d03e6
Drew <drew@openlattice.com>
2019-12-11 17:36:19 -0800

    Ive been told that this is what style looks like

--- src/main/resources/initCitus.sh
+++ src/main/resources/initCitus.sh
@@ -14,23 +14,16 @@ then
   exit 1
 fi
 
-function runPsqlOnNode {
-  docker exec "$1" psql -U postgres -c "$2"
-}
-
-function runPsqlFileOnNodeWithOLDB {
-  docker exec "$1" psql -U postgres -d openlattice -f "$2"
-}
-
-function runPsqlFileOnNode {
-  docker exec "$1" psql -U postgres -f "$2"
+function runPsql {
+  container=$1
+  docker exec "$container" psql -U postgres "$@"
 }
 
 MASTER_EXTERNAL_PORT=5433 COMPOSE_PROJECT_NAME=citus docker-compose -f "$dir"/docker-compose.yml up --scale worker=2 -d
 
 sleep 5
 
-NODES=$(runPsqlOnNode citus_master "SELECT * FROM master_get_active_worker_nodes();"  | grep worker | cut -d ' ' -f 2)
+NODES=$(docker exec citus_master psql -U postgres -c "SELECT * FROM master_get_active_worker_nodes();" | grep worker | cut -d ' ' -f 2)
 
 echo "current nodes: "
 for i in $NODES
@@ -54,20 +47,20 @@ docker cp "$dir"/alter_user.sql citus_master:/opt/
 docker cp "$dir"/alter_user.sql citus_worker_1:/opt/
 docker cp "$dir"/alter_user.sql citus_worker_2:/opt/
 
-runPsqlFileOnNode citus_master /opt/init_ol_db.sql
-runPsqlFileOnNode citus_worker_1 /opt/init_ol_db.sql
-runPsqlFileOnNode citus_worker_2 /opt/init_ol_db.sql
+runPsql citus_master -f /opt/init_ol_db.sql
+runPsql citus_worker_1 -f /opt/init_ol_db.sql
+runPsql citus_worker_2 -f /opt/init_ol_db.sql
 
-runPsqlFileOnNodeWithOLDB citus_master /opt/init_citus.sql
-runPsqlFileOnNodeWithOLDB citus_worker_1 /opt/init_citus.sql
-runPsqlFileOnNodeWithOLDB citus_worker_2 /opt/init_citus.sql
+runPsql citus_master -f /opt/init_citus.sql -d openlattice
+runPsql citus_worker_1 -f /opt/init_citus.sql -d openlattice
+runPsql citus_worker_2 -f /opt/init_citus.sql -d openlattice
 
-runPsqlFileOnNode citus_master /opt/create_user.sql
-runPsqlFileOnNode citus_worker_1 /opt/create_user.sql
-runPsqlFileOnNode citus_worker_2 /opt/create_user.sql
+runPsql citus_master -f /opt/create_user.sql
+runPsql citus_worker_1 -f /opt/create_user.sql
+runPsql citus_worker_2 -f /opt/create_user.sql
 
-runPsqlFileOnNode citus_master /opt/alter_user.sql
-runPsqlFileOnNode citus_worker_1 /opt/alter_user.sql
-runPsqlFileOnNode citus_worker_2 /opt/alter_user.sql
+runPsql citus_master -f /opt/alter_user.sql
+runPsql citus_worker_1 -f /opt/alter_user.sql
+runPsql citus_worker_2 -f /opt/alter_user.sql
 
 docker ps
