e1ec6ce53c77edafec9b275a1c564d36a47d375c
Piper Keyes <piper@openlattice.com>
2020-01-16 11:33:13 -0800

    many stream serializer type ids

--- src/main/java/com/openlattice/hazelcast/serializers/OptionalStreamSerializers.java
+++ src/main/java/com/openlattice/hazelcast/serializers/OptionalStreamSerializers.java
@@ -29,10 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Factory method for stream serializing Guava Optionals in hazelcast.
