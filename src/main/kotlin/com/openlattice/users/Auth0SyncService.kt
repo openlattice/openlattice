428412d9e76f7e84071d55bc9e6beb93d1244de6
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-01-20 10:42:01 -0800

    :thinking face:

--- src/main/kotlin/com/openlattice/users/Auth0SyncService.kt
+++ src/main/kotlin/com/openlattice/users/Auth0SyncService.kt
@@ -50,7 +50,7 @@ class Auth0SyncService(
     fun syncUser(user: User) {
         //Figure out which users need to be added to which organizations.
         //Since we don't want to do O( # organizations ) for each user, we need to lookup organizations on a per user
-        //basis and see if the uscd .er needs to be added.
+        //basis and see if the user needs to be added.
         logger.info("Synchronizing user ${user.id}")
         ensureSecurablePrincipalExists(user)
         val principal = getPrincipal(user)
