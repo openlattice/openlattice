da5693f1b4199b16869316e71615cb8fd8046e17
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-01-22 01:11:01 -0800

    cleanup.

--- src/main/kotlin/com/openlattice/users/Auth0Utils.kt
+++ src/main/kotlin/com/openlattice/users/Auth0Utils.kt
@@ -2,18 +2,20 @@ package com.openlattice.users
 
 import com.auth0.client.mgmt.ManagementAPI
 import com.auth0.client.mgmt.filter.UserFilter
+import com.auth0.exception.Auth0Exception
+import com.auth0.json.mgmt.users.User
 import com.auth0.json.mgmt.users.UsersPage
 import com.auth0.jwt.algorithms.Algorithm
 import com.geekbeast.auth0.*
 import com.openlattice.authentication.Auth0AuthenticationConfiguration
-import org.apache.commons.lang3.NotImplementedException
 import java.security.InvalidAlgorithmParameterException
 
 /**
  *
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
-internal fun getUsersPage(managementApi: ManagementAPI, page: Int, pageSize: Int): UsersPage {
+@Throws(Auth0Exception::class)
+fun getUsersPage(managementApi: ManagementAPI, page: Int, pageSize: Int): UsersPage {
     require(pageSize <= MAX_PAGE_SIZE) { "Requested page size of $pageSize exceeds max page size of $MAX_PAGE_SIZE " }
     return managementApi.users().list(
             UserFilter()
@@ -23,8 +25,18 @@ internal fun getUsersPage(managementApi: ManagementAPI, page: Int, pageSize: Int
     ).execute()
 }
 
-internal fun parseAlgorithm(aac: Auth0AuthenticationConfiguration ) : Algorithm {
-    val algorithm : (String) -> Algorithm  = when(aac.signingAlgorithm) {
+@Throws(Auth0Exception::class)
+fun getUser(managementApi: ManagementAPI, principalId: String): User {
+    return managementApi.users().get(
+            principalId, UserFilter()
+            .withSearchEngine("v3")
+            .withFields("user_id,email,nickname,app_metadata,identities", true)
+            .withPage(0, 100)
+    ).execute()
+}
+
+internal fun parseAlgorithm(aac: Auth0AuthenticationConfiguration): Algorithm {
+    val algorithm: (String) -> Algorithm = when (aac.signingAlgorithm) {
         "HS256" -> Algorithm::HMAC256
         "HS384" -> Algorithm::HMAC384
         "HS512" -> Algorithm::HMAC512
