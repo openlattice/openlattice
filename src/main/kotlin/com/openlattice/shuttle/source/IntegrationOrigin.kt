63bd927ceed6ca972ff589b1b2834cf4ad49bba7
Piper Keyes <piper@openlattice.com>
2020-01-23 10:29:54 -0800

    moved things out

--- src/main/kotlin/com/openlattice/shuttle/source/IntegrationOrigin.kt
+++
@@ -1,7 +0,0 @@
-package com.openlattice.shuttle.source
-
-import java.io.InputStream
-
-abstract class IntegrationOrigin : Sequence<InputStream> {
-    abstract override fun iterator(): Iterator<InputStream>
-}
