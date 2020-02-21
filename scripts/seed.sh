bad2aaf73f16d5109289bf2d361febb3c86e8a7b
jokedurnez <joke.durnez@gmail.com>
2020-02-21 10:10:05 -0800

    adding sql and fixing flight

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,3 +1,4 @@
 pushd ..
-./gradlew :shuttle:run -Dexec.args="--create demo@openlattice.com --flight ../scripts/flights/demohealth.yaml --config ../scripts/flights/integration.yaml --environment LOCAL --datasource demodata --token $TOKEN"
+SQL="select * from public.demo_health"
+./gradlew :shuttle:run -Dexec.args="--create demo@openlattice.com --flight ../scripts/flights/demohealth.yaml --config ../scripts/flights/integration.yaml --environment LOCAL --datasource demodata --sql \"${SQL}\" --token $TOKEN"
 popd
