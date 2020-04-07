e07764c97dae11a5c9a1fc9d18e189e8a1e67fe4
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-04-06 20:19:00 -0700

    Reinstall analysis phonetic plugin.

--- scripts/clear_elasticsearch_indexes.sh
+++ scripts/clear_elasticsearch_indexes.sh
@@ -6,3 +6,4 @@ curl -XDELETE 'http://localhost:9200/association_type_index'
 curl -XDELETE 'http://localhost:9200/app_index'
 curl -XDELETE 'http://localhost:9200/app_type_index'
 curl -XDELETE 'http://localhost:9200/securable_object_*'
+elasticsearch-plugin remove analysis-phonetic && elasticsearch-plugin install analysis-phonetic
