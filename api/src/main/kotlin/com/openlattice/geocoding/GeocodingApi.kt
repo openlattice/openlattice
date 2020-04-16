67b7ac470be38c27376c13fd59915f1b9b6a3398
Katherine Bernstein <katherine@openlattice.com>
2020-04-16 11:26:23 -0700

    add endpoint to get places by id

--- api/src/main/kotlin/com/openlattice/geocoding/GeocodingApi.kt
+++ api/src/main/kotlin/com/openlattice/geocoding/GeocodingApi.kt
@@ -3,7 +3,9 @@ package com.openlattice.geocoding
 import com.google.maps.model.AutocompletePrediction
 import com.google.maps.model.GeocodingResult
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 /**
  *
@@ -16,11 +18,19 @@ interface GeocodingApi {
         const val BASE = SERVICE + CONTROLLER
 
         const val AUTOCOMPLETE = "/autocomplete"
+        const val PLACE = "/place"
         const val GEOCODE = "/geocode"
+
+        const val ID = "id"
+        const val ID_PATH = "/{$ID}"
     }
 
     @POST(BASE + AUTOCOMPLETE)
     fun autocomplete(@Body request: AutocompleteRequest): Array<out AutocompletePrediction>
+
+
+    @GET(BASE + PLACE + ID_PATH)
+    fun geocodePlace(@Path(ID) placeId: String): Array<out GeocodingResult>
 
     @POST(BASE + GEOCODE)
     fun geocode(@Body request: GeocodingRequest): Array<out GeocodingResult>
