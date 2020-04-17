6ef68be7eacc7e07c41abe5cd1d8ea540345183f
Katherine Bernstein <katherine@openlattice.com>
2020-04-17 01:21:17 -0700

    no op stream serializer

--- src/main/kotlin/com/openlattice/hazelcast/serializers/GetNormalEntitySetIdsEntryProcessorStreamSerializer.kt
+++ src/main/kotlin/com/openlattice/hazelcast/serializers/GetNormalEntitySetIdsEntryProcessorStreamSerializer.kt
@@ -1,14 +1,13 @@
 package com.openlattice.hazelcast.serializers
 
 import com.hazelcast.nio.ObjectDataInput
-import com.hazelcast.nio.ObjectDataOutput
 import com.openlattice.edm.processors.GetNormalEntitySetIdsEntryProcessor
 import com.openlattice.hazelcast.StreamSerializerTypeIds
 import org.springframework.stereotype.Component
 
 
 @Component
-class GetNormalEntitySetIdsEntryProcessorStreamSerializer : TestableSelfRegisteringStreamSerializer<GetNormalEntitySetIdsEntryProcessor> {
+class GetNormalEntitySetIdsEntryProcessorStreamSerializer : NoOpSelfRegisteringStreamSerializer<GetNormalEntitySetIdsEntryProcessor>() {
 
     override fun getTypeId(): Int {
         return StreamSerializerTypeIds.GET_NORMAL_ENTITY_SET_IDS_ENTRY_PROCESSOR.ordinal
@@ -18,14 +17,7 @@ class GetNormalEntitySetIdsEntryProcessorStreamSerializer : TestableSelfRegister
         return GetNormalEntitySetIdsEntryProcessor::class.java
     }
 
-    override fun write(out: ObjectDataOutput, `object`: GetNormalEntitySetIdsEntryProcessor) {
-    }
-
     override fun read(`in`: ObjectDataInput): GetNormalEntitySetIdsEntryProcessor {
-        return GetNormalEntitySetIdsEntryProcessor()
-    }
-
-    override fun generateTestValue(): GetNormalEntitySetIdsEntryProcessor {
         return GetNormalEntitySetIdsEntryProcessor()
     }
 }
