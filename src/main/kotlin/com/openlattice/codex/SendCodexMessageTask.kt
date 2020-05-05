e250a3587407b69e883da5a0af087e1074fca204
Katherine Bernstein <katherine@openlattice.com>
2020-05-05 11:08:25 -0700

    documentation??!?!!!?!?!

--- src/main/kotlin/com/openlattice/codex/SendCodexMessageTask.kt
+++ src/main/kotlin/com/openlattice/codex/SendCodexMessageTask.kt
@@ -12,7 +12,11 @@ class SendCodexMessageTask(
 
     override fun run(hazelcastInstance: HazelcastInstance) {
         val twilioQueue = HazelcastQueue.TWILIO.getQueue(hazelcastInstance)
-        twilioQueue.offer(message)
+        twilioQueue.add(message)
+
+        // The twilio queue is not persistent. This sleep call is intended to ensure the message has been processed and
+        // sent before the method returns and the scheduled task is deleted. Twilio rate limits us to 1 message / second
+        // so we check in increments of processing time for a batch of 5 messages to reduce the number of network calls.
         while (twilioQueue.contains(message)) {
             Thread.sleep(5_000)
         }
