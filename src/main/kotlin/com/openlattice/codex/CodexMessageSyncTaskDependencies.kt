3b459ae22a0b9c6ed82f89022401c4684b052e7b
Katherine Bernstein <katherine@openlattice.com>
2020-07-17 23:17:02 -0700

    move services back to datastore, move scheduled logic to CodexService

--- src/main/kotlin/com/openlattice/codex/CodexMessageSyncTaskDependencies.kt
+++
@@ -1,9 +0,0 @@
-package com.openlattice.codex
-
-import com.hazelcast.core.HazelcastInstance
-import com.openlattice.tasks.HazelcastTaskDependencies
-
-class CodexMessageSyncTaskDependencies(
-        val hazelcast: HazelcastInstance,
-        val codexService: CodexService
-) : HazelcastTaskDependencies
