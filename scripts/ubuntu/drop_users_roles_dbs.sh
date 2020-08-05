a76f4c0c5e161bbf3dd01fb3411e788890d0519a
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-08-05 00:21:08 -0700

    Updated scripts for ubuntu.

--- scripts/ubuntu/drop_users_roles_dbs.sh
+++ scripts/ubuntu/drop_users_roles_dbs.sh
@@ -1,6 +1,9 @@
 rm drop_users.sql || :
 rm drop_roles.sql || :
+rm drop_dbs.sql || :
+PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_dbs_gen.sql | tail -n +3 | head -n -2 > drop_dbs.sql
 PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_users_gen.sql | tail -n +3 | head -n -2 > drop_users.sql
 PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_roles_gen.sql | tail -n +3 | head -n -2 > drop_roles.sql
+PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_dbs.sql
 PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_users.sql
 PGPASSWORD=test psql -h localhost -p 5435 -U oltest postgres < drop_roles.sql
