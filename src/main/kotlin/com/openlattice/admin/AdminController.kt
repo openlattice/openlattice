48c277e0c4b5708f82f7a907d59ac65e59af1c7a
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-08-21 23:14:25 -0700

    Add new apis for managing jobs.

--- src/main/kotlin/com/openlattice/admin/AdminController.kt
+++ src/main/kotlin/com/openlattice/admin/AdminController.kt
@@ -19,6 +19,7 @@ import com.openlattice.datastore.services.EdmManager
 import com.openlattice.datastore.services.EntitySetManager
 import com.openlattice.edm.PostgresEdmManager
 import com.openlattice.hazelcast.HazelcastMap
+import com.openlattice.jobs.JobUpdate
 import com.openlattice.notifications.sms.SmsEntitySetInformation
 import com.openlattice.organizations.HazelcastOrganizationService
 import com.openlattice.organizations.Organization
@@ -221,6 +222,16 @@ class AdminController : AdminApi, AuthorizingComponent {
     override fun getJob(@PathVariable(ID) jobId: UUID): Map<UUID,DistributableJob<*>> {
         ensureAdminAccess()
         return jobService.getJobs(listOf(jobId))
+    }
+
+    override fun updateJob(jobId: UUID, update: JobUpdate): Map<UUID, AbstractDistributedJob<*, *>> {
+        ensureAdminAccess()
+        val jobs = setOf(jobId)
+        jobService.updateJob( jobId, update.status )
+
+        if( update.reload ) jobService.reload(jobs, true)
+
+        return jobService.getJobs(jobs)
     }
 
     override fun getAuthorizationManager(): AuthorizationManager {
