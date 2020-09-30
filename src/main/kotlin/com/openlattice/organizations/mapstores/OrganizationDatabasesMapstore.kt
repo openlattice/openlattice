42810b3da7cd44ec6aebc4c6d3ad2f39ae5e1a52
Katherine Bernstein <katherine@openlattice.com>
2020-09-30 17:20:03 -0400

    add a mapstore

---
+++ src/main/kotlin/com/openlattice/organizations/mapstores/OrganizationDatabasesMapstore.kt
@@ -0,0 +1,49 @@
+package com.openlattice.organizations.mapstores
+
+import com.openlattice.hazelcast.HazelcastMap
+import com.openlattice.mapstores.TestDataFactory
+import com.openlattice.postgres.PostgresTable
+import com.openlattice.postgres.ResultSetAdapters
+import com.openlattice.postgres.mapstores.AbstractBasePostgresMapstore
+import com.zaxxer.hikari.HikariDataSource
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.util.*
+
+open class OrganizationDatabasesMapstore(
+        hds: HikariDataSource
+) : AbstractBasePostgresMapstore<UUID, String>
+(HazelcastMap.ORGANIZATION_DATABASES, PostgresTable.ORGANIZATION_DATABASES, hds) {
+
+    override fun bind(ps: PreparedStatement, key: UUID, value: String) {
+        var index = bind(ps, key, 1)
+
+        //create
+        ps.setString(index++, value)
+
+        //update
+        ps.setString(index++, value)
+    }
+
+    override fun bind(ps: PreparedStatement, key: UUID, offset: Int): Int {
+        var index = offset
+        ps.setObject(index++, key)
+        return index
+    }
+
+    override fun mapToKey(rs: ResultSet?): UUID {
+        return ResultSetAdapters.id(rs)
+    }
+
+    override fun mapToValue(rs: ResultSet?): String {
+        return ResultSetAdapters.name(rs)
+    }
+
+    override fun generateTestKey(): UUID {
+        return UUID.randomUUID()
+    }
+
+    override fun generateTestValue(): String {
+        return TestDataFactory.randomAlphanumeric(10)
+    }
+}
