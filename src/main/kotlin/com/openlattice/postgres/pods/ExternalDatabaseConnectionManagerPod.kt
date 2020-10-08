6155e33bf623b84b8839dc191a238cdfb5bdd7bc
Hristo Oskov <hristo@openlattice.com>
2020-10-08 09:32:20 -0700

    ExternalDatabaseConnectionManagerPod, not TransporterPod

--- src/main/kotlin/com/openlattice/postgres/pods/ExternalDatabaseConnectionManagerPod.kt
+++ src/main/kotlin/com/openlattice/postgres/pods/ExternalDatabaseConnectionManagerPod.kt
@@ -3,7 +3,6 @@ package com.openlattice.postgres.pods
 import com.hazelcast.core.HazelcastInstance
 import com.openlattice.assembler.AssemblerConfiguration
 import com.openlattice.postgres.external.ExternalDatabaseConnectionManager
-import com.openlattice.transporter.pods.TransporterPod
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,7 +22,7 @@ class ExternalDatabaseConnectionManagerPod {
 
     @Bean
     fun externalDatabaseConnectionManager(): ExternalDatabaseConnectionManager {
-        LoggerFactory.getLogger(TransporterPod::class.java).info("Constructing ExternalDatabaseConnectionManager")
+        LoggerFactory.getLogger(ExternalDatabaseConnectionManagerPod::class.java).info("Constructing ExternalDatabaseConnectionManager")
         return ExternalDatabaseConnectionManager(assemblerConfiguration, hazelcastInstance)
     }
 }
