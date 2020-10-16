dd5c694939d16bf1a83a2f1a19ba9f413f0931ca
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-10-15 21:29:28 -0700

    Fix formatting.

--- api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
+++ api/src/main/kotlin/com/openlattice/admin/AdminApi.kt
@@ -95,9 +95,9 @@ interface AdminApi {
 
     @GET(BASE + JOBS + ID_PATH)
     fun getJob(@Path(ID) jobId: UUID): Map<UUID, DistributableJob<*>>
-    
-    @PATCH(BASE + JOBS )
-    fun createJobs( @Body jobs: List<DistributableJob<*>>): List<UUID>
+
+    @PATCH(BASE + JOBS)
+    fun createJobs(@Body jobs: List<DistributableJob<*>>): List<UUID>
 
     @PATCH(BASE + JOBS + ID_PATH)
     fun updateJob(@Path(ID) jobId: UUID, @Body update: JobUpdate): Map<UUID, DistributableJob<*>>
