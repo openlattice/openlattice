b10e928b22b893902f687e8de8657ac66d829e8b
Katherine Bernstein <katherine@openlattice.com>
2020-10-22 23:51:38 -0400

    Include EntityTypeCollections and Apps in EdmSyncTask

--- src/main/kotlin/com/openlattice/edm/tasks/EdmSyncInitializerDependencies.kt
+++ src/main/kotlin/com/openlattice/edm/tasks/EdmSyncInitializerDependencies.kt
@@ -1,9 +1,11 @@
 package com.openlattice.edm.tasks
 
+import com.hazelcast.core.HazelcastInstance
 import com.openlattice.datastore.services.EdmManager
 import com.openlattice.tasks.HazelcastTaskDependencies
 
 data class EdmSyncInitializerDependencies(
         val edmManager: EdmManager,
-        val active: Boolean
+        val active: Boolean,
+        val hazelcast: HazelcastInstance
 ) : HazelcastTaskDependencies
