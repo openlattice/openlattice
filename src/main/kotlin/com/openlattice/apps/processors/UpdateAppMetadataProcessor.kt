f2d3952cc83daab386cbd358c6730e4ef6ce349d
Katherine Bernstein <katherine@openlattice.com>
2020-10-22 20:30:41 -0400

    return boolean instead of unit from EPs

--- src/main/kotlin/com/openlattice/apps/processors/UpdateAppMetadataProcessor.kt
+++ src/main/kotlin/com/openlattice/apps/processors/UpdateAppMetadataProcessor.kt
@@ -6,25 +6,26 @@ import com.openlattice.edm.requests.MetadataUpdate
 import java.util.*
 import kotlin.collections.MutableMap.MutableEntry
 
-data class UpdateAppMetadataProcessor(val update: MetadataUpdate) : AbstractRhizomeEntryProcessor<UUID, App?, Unit>() {
+data class UpdateAppMetadataProcessor(val update: MetadataUpdate) : AbstractRhizomeEntryProcessor<UUID, App?, Boolean>() {
 
-    override fun process(entry: MutableEntry<UUID, App?>) {
-        val app = entry.value
-        if (app != null) {
-            if (update.title.isPresent) {
-                app.title = update.title.get()
-            }
-            if (update.description.isPresent) {
-                app.description = update.description.get()
-            }
-            if (update.name.isPresent) {
-                app.name = update.name.get()
-            }
-            if (update.url.isPresent) {
-                app.url = update.url.get()
-            }
-            entry.setValue(app)
+    override fun process(entry: MutableEntry<UUID, App?>): Boolean {
+        val app = entry.value ?: return false
+
+        if (update.title.isPresent) {
+            app.title = update.title.get()
         }
+        if (update.description.isPresent) {
+            app.description = update.description.get()
+        }
+        if (update.name.isPresent) {
+            app.name = update.name.get()
+        }
+        if (update.url.isPresent) {
+            app.url = update.url.get()
+        }
+        entry.setValue(app)
+
+        return true
     }
 
 }
