7b0c9b47614b57141b244eb9783e82760caad287
Katherine Bernstein <katherine@openlattice.com>
2020-10-29 14:39:49 -0400

    ConductorConfiguration.getKey recursive loop

--- src/main/kotlin/com/openlattice/conductor/rpc/ConductorConfiguration.kt
+++ src/main/kotlin/com/openlattice/conductor/rpc/ConductorConfiguration.kt
@@ -1,6 +1,5 @@
 package com.openlattice.conductor.rpc
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.kryptnostic.rhizome.configuration.Configuration
 import com.kryptnostic.rhizome.configuration.ConfigurationKey
@@ -21,15 +20,15 @@ data class ConductorConfiguration(
         @JsonProperty("searchConfiguration") val searchConfiguration: SearchConfiguration,
         @JsonProperty("bootstrap-connection") val connection: Optional<Set<String>>
 ) : Configuration {
-    @JsonIgnore
-    override fun getKey(): ConfigurationKey {
-        return key
-    }
 
     companion object {
         @JvmStatic
-        fun key(): ConfigurationKey {
-            return key
-        }
+        @get:JvmName("key")
+        val key = SimpleConfigurationKey("conductor.yaml")
     }
+
+    override fun getKey(): ConfigurationKey {
+        return ConductorConfiguration.key
+    }
+    
 }
