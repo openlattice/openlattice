9cdafd348468bcec9638cac40416623bae609d8a
Drew <drew@openlattice.com>
2021-02-04 12:54:47 -0500

    unused

--- conductor-client/src/main/kotlin/com/openlattice/postgres/external/Schemas.kt
+++ conductor-client/src/main/kotlin/com/openlattice/postgres/external/Schemas.kt
@@ -14,7 +14,6 @@ package com.openlattice.postgres.external
 enum class Schemas(val label: String) {
     INTEGRATIONS_SCHEMA("integrations"),
     OPENLATTICE_SCHEMA("openlattice"),
-    TRANSPORTED_VIEWS_SCHEMA("ol"),
     PUBLIC_SCHEMA("public"),
     TRANSPORTER_SCHEMA("transporter"),
     ASSEMBLED_ENTITY_SETS("entitysets"),
