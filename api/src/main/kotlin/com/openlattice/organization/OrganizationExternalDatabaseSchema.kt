e36c33d48305e18bedb47ae4048cce29a09afbfb
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-02-03 21:58:53 -1000

    Update api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseSchema.kt
    Co-authored-by: Hristo Oskov <UnsungHero97@users.noreply.github.com>

--- api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseSchema.kt
+++ api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseSchema.kt
@@ -16,7 +16,7 @@ import java.util.*
  * @param description An optional description of the schema
  * @param organizationId The id of the organization that owns this schema
  * @param dataSourceId The id of the data source to which this table belongs.
- * @param externalId An datasource specific uniqued string identifier for a table.
+ * @param externalId A datasource specific unique string identifier for a schema.
  */
 
 class OrganizationExternalDatabaseSchema
