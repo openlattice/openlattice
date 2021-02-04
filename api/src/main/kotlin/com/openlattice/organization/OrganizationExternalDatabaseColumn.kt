3eb0ec1abb82c5dd54e65d918d314cd84d7ce56d
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-02-03 21:47:15 -1000

    Fix imports.

--- api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseColumn.kt
+++ api/src/main/kotlin/com/openlattice/organization/OrganizationExternalDatabaseColumn.kt
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.openlattice.authorization.securable.AbstractSecurableObject
 import com.openlattice.authorization.securable.SecurableObjectType
 import com.openlattice.client.serialization.SerializationConstants
-import com.openlattice.organizations.external.SqlDataType
 import java.util.*
-import com.openlattice.postgres.PostgresDatatype
 
 /**
  * Creates a securable object for an organization's entire database
