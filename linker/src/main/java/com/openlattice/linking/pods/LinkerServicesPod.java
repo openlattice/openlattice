2e47f593e8e015fc71d24619af566f28180907af
Drew <drew@openlattice.com>
2021-02-12 18:22:46 -0500

    so slow

--- linker/src/main/java/com/openlattice/linking/pods/LinkerServicesPod.java
+++ linker/src/main/java/com/openlattice/linking/pods/LinkerServicesPod.java
@@ -200,7 +200,7 @@ public class LinkerServicesPod {
                 principalService(),
                 organizationsManager(),
                 dbcs(),
-                extDatabasePermsManager,
+                externalDatabasePermissionsManager(),
                 eventBus,
                 metricRegistry
         );
