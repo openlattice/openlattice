8ae49e6cf54ae5297129382ffd3f1e49352bf941
Katherine Bernstein <katherine@openlattice.com>
2021-02-26 21:01:19 -0500

    toList not necessary

--- conductor-client/src/main/kotlin/com/openlattice/organizations/ExternalDatabaseManagementService.kt
+++ conductor-client/src/main/kotlin/com/openlattice/organizations/ExternalDatabaseManagementService.kt
@@ -467,7 +467,6 @@ class ExternalDatabaseManagementService(
         val columnToUserToPrivileges = BasePostgresIterable(StatementHolderSupplier(orgHDS, columnPrivilegesSql)) { rs ->
             columnName(rs) to (user(rs) to PostgresPrivileges.valueOf(privilegeType(rs).toUpperCase()))
         }
-                .toList()
                 .groupBy { columnNameToAclKey.getValue(it.first) }
                 .mapValues {
                     it.value.map { pair -> pair.second }
