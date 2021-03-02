a62ef0b4eebd22c9f773d5b4c85d4640d4b5d101
Drew <drew@openlattice.com>
2021-03-01 20:54:33 -0500

    d e r p

--- conductor-client/src/main/kotlin/com/openlattice/hazelcast/serializers/UUIDStreamSerializer.kt
+++ conductor-client/src/main/kotlin/com/openlattice/hazelcast/serializers/UUIDStreamSerializer.kt
@@ -2,15 +2,12 @@ package com.openlattice.hazelcast.serializers
 
 import com.hazelcast.nio.ObjectDataInput
 import com.hazelcast.nio.ObjectDataOutput
-import com.openlattice.hazelcast.StreamSerializerTypeIds
-import org.springframework.stereotype.Component
-import java.util.UUID
+import java.util.*
 
 /**
  * @author Drew Bailey (drew@openlattice.com)
  */
-@Component
-class UUIDStreamSerializer: TestableSelfRegisteringStreamSerializer<UUID> {
+class UUIDStreamSerializer {
     companion object {
         @JvmStatic
         fun serialize(out: ObjectDataOutput, `object`: UUID) {
@@ -22,25 +19,5 @@ class UUIDStreamSerializer: TestableSelfRegisteringStreamSerializer<UUID> {
         fun deserialize(`in`: ObjectDataInput): UUID {
             return UUID( `in`.readLong(), `in`.readLong())
         }
-    }
-
-    override fun getClazz(): Class<out UUID> {
-        return UUID::class.java
-    }
-
-    override fun write(out: ObjectDataOutput, `object`: UUID) {
-        serialize(out, `object`)
-    }
-
-    override fun read(`in`: ObjectDataInput): UUID {
-        return deserialize(`in`)
-    }
-
-    override fun generateTestValue(): UUID {
-        return UUID.randomUUID()
-    }
-
-    override fun getTypeId(): Int {
-        return StreamSerializerTypeIds.UUID.ordinal
     }
 }
