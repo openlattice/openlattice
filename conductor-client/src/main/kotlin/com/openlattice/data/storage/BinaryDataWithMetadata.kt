7d179e8de50b0cc28448e52db36c22748c26f3b0
Kat <bernstein.katherine@gmail.com>
2021-04-06 10:52:30 -0400

    bring it back

--- conductor-client/src/main/kotlin/com/openlattice/data/storage/BinaryDataWithMetadata.kt
+++ conductor-client/src/main/kotlin/com/openlattice/data/storage/BinaryDataWithMetadata.kt
@@ -48,4 +48,24 @@ data class BinaryDataWithMetadata(
             )
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BinaryDataWithMetadata
+
+        if (contentType != other.contentType) return false
+        if (!`data`.contentEquals(other.`data`)) return false
+        if (contentDisposition != other.contentDisposition) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = contentType.hashCode()
+        result = 31 * result + `data`.contentHashCode()
+        result = 31 * result + (contentDisposition?.hashCode() ?: 0)
+        return result
+    }
 }
