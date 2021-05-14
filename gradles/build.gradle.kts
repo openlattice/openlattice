09c64da427fd662b7fa6756f489c30064a592fcb
Drew <drew@openlattice.com>
2021-05-14 16:17:37 -0400

    updates for new branch structure

---
+++ gradles/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+apply("./openlattice.gradle")
