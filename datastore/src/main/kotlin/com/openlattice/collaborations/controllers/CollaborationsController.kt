ff157f98fd8c3c0e55b9d3f7277452dbcc23c81b
Hristo Oskov <hristo@openlattice.com>
2021-05-27 09:49:16 -0700

    update function names in CollaborationsApi and CollaborationsController, update docs

--- datastore/src/main/kotlin/com/openlattice/collaborations/controllers/CollaborationsController.kt
+++ datastore/src/main/kotlin/com/openlattice/collaborations/controllers/CollaborationsController.kt
@@ -6,17 +6,17 @@ import com.openlattice.authorization.securable.SecurableObjectType
 import com.openlattice.collaborations.Collaboration
 import com.openlattice.collaborations.CollaborationService
 import com.openlattice.collaborations.CollaborationsApi
+import com.openlattice.collaborations.CollaborationsApi.Companion.COLLABORATION_ID_PARAM
+import com.openlattice.collaborations.CollaborationsApi.Companion.COLLABORATION_ID_PATH
 import com.openlattice.collaborations.CollaborationsApi.Companion.CONTROLLER
 import com.openlattice.collaborations.CollaborationsApi.Companion.DATABASE_PATH
-import com.openlattice.collaborations.CollaborationsApi.Companion.ID
-import com.openlattice.collaborations.CollaborationsApi.Companion.ID_PATH_PARAM
+import com.openlattice.collaborations.CollaborationsApi.Companion.DATA_SETS_PATH
+import com.openlattice.collaborations.CollaborationsApi.Companion.DATA_SET_ID_PARAM
+import com.openlattice.collaborations.CollaborationsApi.Companion.DATA_SET_ID_PATH
 import com.openlattice.collaborations.CollaborationsApi.Companion.ORGANIZATIONS_PATH
-import com.openlattice.collaborations.CollaborationsApi.Companion.ORGANIZATION_ID
-import com.openlattice.collaborations.CollaborationsApi.Companion.ORGANIZATION_ID_PATH_PARAM
+import com.openlattice.collaborations.CollaborationsApi.Companion.ORGANIZATION_ID_PARAM
+import com.openlattice.collaborations.CollaborationsApi.Companion.ORGANIZATION_ID_PATH
 import com.openlattice.collaborations.CollaborationsApi.Companion.PROJECT_PATH
-import com.openlattice.collaborations.CollaborationsApi.Companion.TABLES_PATH
-import com.openlattice.collaborations.CollaborationsApi.Companion.TABLE_ID
-import com.openlattice.collaborations.CollaborationsApi.Companion.TABLE_ID_PATH_PARAM
 import com.openlattice.organizations.OrganizationDatabase
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.springframework.http.MediaType
@@ -28,8 +28,9 @@ import javax.inject.Inject
 @RestController
 @RequestMapping(CONTROLLER)
 @SuppressFBWarnings(
-        value = ["RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE"],
-        justification = "Allowing redundant kotlin null check on lateinit variables")
+    value = ["RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE"],
+    justification = "Allowing redundant kotlin null check on lateinit variables"
+)
 class CollaborationsController : AuthorizingComponent, CollaborationsApi {
 
     @Inject
@@ -39,115 +40,162 @@ class CollaborationsController : AuthorizingComponent, CollaborationsApi {
     private lateinit var collaborationService: CollaborationService
 
     @Timed
-    @GetMapping(value = ["", "/"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(
+        value = ["", "/"],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
     override fun getCollaborations(): Iterable<Collaboration> {
         val authorizedCollaborationIds = getAllAuthorizedCollaborationIds()
         return collaborationService.getCollaborations(authorizedCollaborationIds).values
     }
 
     @Timed
-    @PostMapping(value = ["", "/"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(
+        value = [COLLABORATION_ID_PATH],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getCollaboration(@PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID): Collaboration {
+        ensureReadAccess(AclKey(collaborationId))
+        return collaborationService.getCollaboration(collaborationId)
+    }
+
+    @Timed
+    @GetMapping(
+        value = [ORGANIZATIONS_PATH + ORGANIZATION_ID_PATH],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getCollaborationsWithOrganization(
+        @PathVariable(ORGANIZATION_ID_PARAM) organizationId: UUID
+    ): Iterable<Collaboration> {
+        ensureReadAccess(AclKey(organizationId))
+        val collaborations = collaborationService.getCollaborationsIncludingOrg(organizationId)
+        val authorizedIds = filterToAuthorizedIds(collaborations.map { it.id })
+        return collaborations.filter { authorizedIds.contains(it.id) }
+    }
+
+    @Timed
+    @PostMapping(
+        value = ["", "/"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
     override fun createCollaboration(@RequestBody collaboration: Collaboration): UUID {
         return collaborationService.createCollaboration(collaboration, Principals.getCurrentUser())
     }
 
     @Timed
-    @GetMapping(value = [ID_PATH_PARAM], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getCollaboration(@PathVariable(ID) id: UUID): Collaboration {
-        ensureReadAccess(AclKey(id))
-        return collaborationService.getCollaboration(id)
+    @DeleteMapping(
+        value = [COLLABORATION_ID_PATH]
+    )
+    override fun deleteCollaboration(@PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID) {
+        ensureOwnerAccess(AclKey(collaborationId))
+        collaborationService.deleteCollaboration(collaborationId)
     }
 
     @Timed
-    @GetMapping(value = [ORGANIZATIONS_PATH + ORGANIZATION_ID_PATH_PARAM], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getCollaborationsIncludingOrganization(@PathVariable(ORGANIZATION_ID) organizationId: UUID): Iterable<Collaboration> {
-        ensureReadAccess(AclKey(organizationId))
-        val collaborations = collaborationService.getCollaborationsIncludingOrg(organizationId)
-
-        val authorizedIds = filterToAuthorizedIds(collaborations.map { it.id })
-
-        return collaborations.filter { authorizedIds.contains(it.id) }
+    @PatchMapping(
+        value = [COLLABORATION_ID_PATH + ORGANIZATIONS_PATH],
+        consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun addOrganizationsToCollaboration(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID,
+        @RequestBody organizationIds: Set<UUID>
+    ) {
+        ensureOwnerAccess(AclKey(collaborationId))
+        collaborationService.addOrganizationIdsToCollaboration(collaborationId, organizationIds)
     }
 
     @Timed
-    @DeleteMapping(value = [ID_PATH_PARAM])
-    override fun deleteCollaboration(@PathVariable(ID) id: UUID) {
-        ensureOwnerAccess(AclKey(id))
-        collaborationService.deleteCollaboration(id)
+    @DeleteMapping(
+        value = [COLLABORATION_ID_PATH + ORGANIZATIONS_PATH],
+        consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun removeOrganizationsFromCollaboration(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID,
+        @RequestBody organizationIds: Set<UUID>
+    ) {
+        ensureOwnerAccess(AclKey(collaborationId))
+        collaborationService.removeOrganizationIdsFromCollaboration(collaborationId, organizationIds)
     }
 
     @Timed
-    @PostMapping(value = [ID_PATH_PARAM + ORGANIZATIONS_PATH], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    override fun addOrganizationIdsToCollaboration(@PathVariable(ID) id: UUID, @RequestBody organizationIds: Set<UUID>) {
-        ensureOwnerAccess(AclKey(id))
-        collaborationService.addOrganizationIdsToCollaboration(id, organizationIds)
+    @GetMapping(
+        value = [COLLABORATION_ID_PATH + DATABASE_PATH],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getCollaborationDatabaseInfo(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID
+    ): OrganizationDatabase {
+        ensureReadAccess(AclKey(collaborationId))
+        return collaborationService.getDatabaseInfo(collaborationId)
     }
 
     @Timed
-    @DeleteMapping(value = [ID_PATH_PARAM + ORGANIZATIONS_PATH], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    override fun removeOrganizationIdsFromCollaboration(@PathVariable(ID) id: UUID, @RequestBody organizationIds: Set<UUID>) {
-        ensureOwnerAccess(AclKey(id))
-        collaborationService.removeOrganizationIdsFromCollaboration(id, organizationIds)
+    @PatchMapping(
+        value = [COLLABORATION_ID_PATH + DATABASE_PATH],
+        consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun renameCollaborationDatabase(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID,
+        @RequestBody name: String
+    ) {
+        ensureOwnerAccess(AclKey(collaborationId))
+        collaborationService.renameDatabase(collaborationId, name)
     }
 
     @Timed
-    @GetMapping(value = [ID_PATH_PARAM + DATABASE_PATH], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getCollaborationDatabaseInfo(@PathVariable(ID) id: UUID): OrganizationDatabase {
-        ensureReadAccess(AclKey(id))
-        return collaborationService.getDatabaseInfo(id)
-    }
-
-    @Timed
-    @PatchMapping(value = [ID_PATH_PARAM + DATABASE_PATH], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    override fun renameDatabase(@PathVariable(ID) id: UUID, @RequestBody newDatabaseName: String) {
-        ensureOwnerAccess(AclKey(id))
-        collaborationService.renameDatabase(id, newDatabaseName)
-
-    }
-
-    @Timed
-    @GetMapping(value = [ID_PATH_PARAM + PROJECT_PATH + ORGANIZATION_ID_PATH_PARAM + TABLE_ID_PATH_PARAM])
-    override fun projectTableToCollaboration(
-            @PathVariable(ID) collaborationId: UUID,
-            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
-            @PathVariable(TABLE_ID) tableId: UUID
+    @PatchMapping(
+        value = [COLLABORATION_ID_PATH + PROJECT_PATH + ORGANIZATION_ID_PATH + DATA_SET_ID_PATH]
+    )
+    override fun addDataSetToCollaboration(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID,
+        @PathVariable(ORGANIZATION_ID_PARAM) organizationId: UUID,
+        @PathVariable(DATA_SET_ID_PARAM) dataSetId: UUID
     ) {
         ensureReadAccess(AclKey(collaborationId))
         ensureReadAccess(AclKey(organizationId))
-        ensureOwnerAccess(AclKey(tableId))
-
-        collaborationService.projectTableToCollaboration(collaborationId, organizationId, tableId)
+        ensureOwnerAccess(AclKey(dataSetId))
+        collaborationService.projectTableToCollaboration(collaborationId, organizationId, dataSetId)
     }
 
     @Timed
-    @DeleteMapping(value = [ID_PATH_PARAM + PROJECT_PATH + ORGANIZATION_ID_PATH_PARAM + TABLE_ID_PATH_PARAM])
-    override fun removeProjectedTableFromCollaboration(
-            @PathVariable(ID) collaborationId: UUID,
-            @PathVariable(ORGANIZATION_ID) organizationId: UUID,
-            @PathVariable(TABLE_ID) tableId: UUID
+    @DeleteMapping(
+        value = [COLLABORATION_ID_PATH + PROJECT_PATH + ORGANIZATION_ID_PATH + DATA_SET_ID_PATH]
+    )
+    override fun removeDataSetFromCollaboration(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID,
+        @PathVariable(ORGANIZATION_ID_PARAM) organizationId: UUID,
+        @PathVariable(DATA_SET_ID_PARAM) dataSetId: UUID
     ) {
         ensureReadAccess(AclKey(collaborationId))
         ensureReadAccess(AclKey(organizationId))
-        ensureOwnerAccess(AclKey(tableId))
-
-        collaborationService.removeProjectedTableFromCollaboration(collaborationId, organizationId, tableId)
-
+        ensureOwnerAccess(AclKey(dataSetId))
+        collaborationService.removeProjectedTableFromCollaboration(collaborationId, organizationId, dataSetId)
     }
 
     @Timed
-    @GetMapping(value = [ORGANIZATIONS_PATH + ORGANIZATION_ID_PATH_PARAM + TABLES_PATH], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getProjectedTablesInOrganization(@PathVariable(ORGANIZATION_ID) organizationId: UUID): Map<UUID, List<UUID>> {
+    @GetMapping(
+        value = [ORGANIZATIONS_PATH + ORGANIZATION_ID_PATH + DATA_SETS_PATH],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getOrganizationCollaborationDataSets(
+        @PathVariable(ORGANIZATION_ID_PARAM) organizationId: UUID
+    ): Map<UUID, List<UUID>> {
         // auth checks on org and collabs done inside here
-        val collaborationIds = getCollaborationsIncludingOrganization(organizationId).map { it.id }
-
+        val collaborationIds = getCollaborationsWithOrganization(organizationId).map { it.id }
         return collaborationService.getProjectedTableIdsInCollaborationsAndOrganizations(collaborationIds, setOf(organizationId)) {
             it.key.collaborationId
         }
     }
 
     @Timed
-    @GetMapping(value = [ID_PATH_PARAM + TABLES_PATH], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getProjectedTablesInCollaboration(@PathVariable(ID) collaborationId: UUID): Map<UUID, List<UUID>> {
+    @GetMapping(
+        value = [COLLABORATION_ID_PATH + DATA_SETS_PATH],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getCollaborationDataSets(
+        @PathVariable(COLLABORATION_ID_PARAM) collaborationId: UUID
+    ): Map<UUID, List<UUID>> {
         ensureReadAccess(AclKey(collaborationId))
 
         val authorizedOrgIds = filterToAuthorizedIds(collaborationService.getCollaboration(collaborationId).organizationIds)
@@ -158,31 +206,33 @@ class CollaborationsController : AuthorizingComponent, CollaborationsApi {
     }
 
     @Timed
-    @PostMapping(value = [TABLES_PATH], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
-    override fun getProjectionCollaborationsForTables(@RequestBody tableIds: Set<UUID>): Map<UUID, List<UUID>> {
-        val authorizedTableIds = filterToAuthorizedIds(tableIds)
+    @PostMapping(
+        value = [DATA_SETS_PATH],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    override fun getCollaborationsWithDataSets(@RequestBody dataSetIds: Set<UUID>): Map<UUID, List<UUID>> {
+        val authorizedTableIds = filterToAuthorizedIds(dataSetIds)
         val authorizedCollaborationIds = getAllAuthorizedCollaborationIds()
-
         return collaborationService.getCollaborationIdsWithProjectionsForTables(authorizedTableIds, authorizedCollaborationIds)
     }
 
     private fun getAllAuthorizedCollaborationIds(): Set<UUID> {
         return authorizations.getAuthorizedObjectsOfType(
-                Principals.getCurrentPrincipals(),
-                SecurableObjectType.Collaboration,
-                EnumSet.of(Permission.READ)
+            Principals.getCurrentPrincipals(),
+            SecurableObjectType.Collaboration,
+            EnumSet.of(Permission.READ)
         ).map { it.first() }.collect(Collectors.toSet())
     }
 
     private fun filterToAuthorizedIds(ids: Iterable<UUID>): Set<UUID> {
         return authorizations.accessChecksForPrincipals(
-                ids.map { AccessCheck(AclKey(it), EnumSet.of(Permission.READ)) }.toSet(),
-                Principals.getCurrentPrincipals()
+            ids.map { AccessCheck(AclKey(it), EnumSet.of(Permission.READ)) }.toSet(),
+            Principals.getCurrentPrincipals()
         ).filter { it.permissions.getValue(Permission.READ) }.map { it.aclKey.first() }.collect(Collectors.toSet())
     }
 
     override fun getAuthorizationManager(): AuthorizationManager {
         return authorizations
     }
-
 }
