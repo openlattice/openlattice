02191d1268fc26c55710fc8c73d17ee9c92c6cf3
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-06-10 19:17:48 -0700

    Finally fix test.

--- conductor-client/src/test/kotlin/com/openlattice/hazelcast/serializers/DistributableJobStreamSerializerTest.kt
+++ conductor-client/src/test/kotlin/com/openlattice/hazelcast/serializers/DistributableJobStreamSerializerTest.kt
@@ -2,9 +2,13 @@ package com.openlattice.hazelcast.serializers
 
 import com.geekbeast.rhizome.jobs.DistributableJob
 import com.hazelcast.core.HazelcastInstance
+import com.hazelcast.map.IMap
 import com.kryptnostic.rhizome.hazelcast.serializers.AbstractStreamSerializerTest
+import com.openlattice.data.integration.Entity
 import com.openlattice.data.storage.DataSourceResolver
+import com.openlattice.edm.EntitySet
 import com.openlattice.graph.partioning.RepartitioningJob
+import com.openlattice.hazelcast.HazelcastMap
 import com.openlattice.jdbc.DataSourceManager
 import com.zaxxer.hikari.HikariDataSource
 import org.apache.commons.lang3.RandomUtils
@@ -19,10 +23,14 @@ class DistributableJobStreamSerializerTest :
     AbstractStreamSerializerTest<DistributableJobStreamSerializer, DistributableJob<*>>() {
     override fun createSerializer(): DistributableJobStreamSerializer {
         val ss = DistributableJobStreamSerializer()
+        
         val dataSourceManager = Mockito.mock(DataSourceManager::class.java)
         val hds = Mockito.mock(HikariDataSource::class.java)
+        val hz = Mockito.mock(HazelcastInstance::class.java)
+        val m = Mockito.mock(IMap::class.java) as IMap<UUID, EntitySet>
+        Mockito.`when`(hz.getMap<UUID, EntitySet>(HazelcastMap.ENTITY_SETS.name)).thenReturn(m)
         Mockito.`when`(dataSourceManager.getDefaultDataSource()).thenReturn(hds)
-        val dsr = DataSourceResolver(Mockito.mock(HazelcastInstance::class.java), dataSourceManager)
+        val dsr = DataSourceResolver(hz, dataSourceManager)
         ss.setDataSourceResolver(dsr)
         return ss
     }
