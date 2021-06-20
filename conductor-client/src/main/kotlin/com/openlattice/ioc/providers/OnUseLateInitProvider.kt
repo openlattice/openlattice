5aa1bb5de050e5d4a65f17ca32ff860618d1f3a2
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-06-20 13:37:32 -1000

    Will wait until properties are initialized.

--- conductor-client/src/main/kotlin/com/openlattice/ioc/providers/OnUseLateInitProvider.kt
+++ conductor-client/src/main/kotlin/com/openlattice/ioc/providers/OnUseLateInitProvider.kt
@@ -7,6 +7,12 @@ import com.openlattice.hazelcast.serializers.decorators.DataGraphAware
 import com.openlattice.hazelcast.serializers.decorators.MetastoreAware
 import com.openlattice.ids.HazelcastIdGenerationService
 import com.openlattice.ids.IdGenerationServiceDependent
+import java.util.concurrent.CountDownLatch
+
+private const val RESOLVER = "RESOLVER"
+private const val ID_SERVICE = "idService"
+private const val BYTE_BLOB_DATA_MANAGER = "bBDM"
+private const val DATA_GRAPH_SERVICE = "dGS"
 
 /**
  *
@@ -18,25 +24,38 @@ class OnUseLateInitProvider : LateInitProvider, MetastoreAware, DataGraphAware, 
     private lateinit var _byteBlobDataManager: ByteBlobDataManager
     private lateinit var _dataGraphService: DataGraphService
 
+    private val latches = mutableMapOf<String, CountDownLatch>()
+
     override val resolver: DataSourceResolver
-        get() = _resolver
+        get() = latchedGet(RESOLVER, _resolver)
     override val idService: HazelcastIdGenerationService
-        get() = _idService
+        get() = latchedGet(ID_SERVICE, _idService)
     override val byteBlobDataManager: ByteBlobDataManager
-        get() = _byteBlobDataManager
+        get() = latchedGet(BYTE_BLOB_DATA_MANAGER, _byteBlobDataManager)
     override val dataGraphService: DataGraphService
-        get() = _dataGraphService
+        get() = latchedGet(DATA_GRAPH_SERVICE, _dataGraphService)
 
     override fun init(idService: HazelcastIdGenerationService) {
         _idService = idService
+        countdown(ID_SERVICE)
     }
 
     override fun setDataGraphService(dataGraphService: DataGraphService) {
         _dataGraphService = dataGraphService
+        countdown(DATA_GRAPH_SERVICE)
     }
 
     override fun setDataSourceResolver(resolver: DataSourceResolver) {
         _resolver = resolver
+        countdown(RESOLVER)
     }
 
+    private fun countdown(key: String) {
+        latches.getOrPut(key) { CountDownLatch(1) }.countDown()
+    }
+
+    private fun <T> latchedGet(key: String, retval: T): T {
+        latches.getOrPut(key) { CountDownLatch(1) }.await()
+        return retval
+    }
 }
