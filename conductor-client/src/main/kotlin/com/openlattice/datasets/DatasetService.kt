cfae385ffa8d8969856be1b1c55e9a5fc36623c0
Hristo Oskov <hristo@openlattice.com>
2021-06-28 17:30:34 -0700

    use search service for indexing instead of event bus

--- conductor-client/src/main/kotlin/com/openlattice/datasets/DatasetService.kt
+++ conductor-client/src/main/kotlin/com/openlattice/datasets/DatasetService.kt
@@ -1,23 +1,22 @@
 package com.openlattice.datasets
 
-import com.google.common.eventbus.EventBus
 import com.hazelcast.core.HazelcastInstance
 import com.hazelcast.query.Predicates
 import com.openlattice.authorization.AclKey
 import com.openlattice.authorization.securable.SecurableObjectType
-import com.openlattice.datasets.events.DatasetColumnsUpdatedEvent
-import com.openlattice.datasets.events.DatasetCreatedEvent
-import com.openlattice.datasets.events.DatasetDeletedEvent
 import com.openlattice.edm.EntitySet
 import com.openlattice.hazelcast.HazelcastMap
 import com.openlattice.organization.ExternalTable
 import com.openlattice.organizations.mapstores.ExternalTablesMapstore
 import com.openlattice.postgres.mapstores.EntitySetMapstore
+import com.openlattice.search.SearchService
+import org.springframework.stereotype.Service
 import java.util.*
 
+@Service
 class DatasetService(
-        val hazelcast: HazelcastInstance,
-        val eventBus: EventBus
+    hazelcast: HazelcastInstance,
+    val searchService: SearchService
 ) {
 
     private val entitySets = HazelcastMap.ENTITY_SETS.getMap(hazelcast)
@@ -45,13 +44,13 @@ class DatasetService(
 
     fun updateObjectMetadata(aclKey: AclKey, update: SecurableObjectMetadataUpdate): Boolean {
         val success = objectMetadata.executeOnKey(aclKey, SecurableObjectMetadataUpdateEntryProcessor(update))
-        indexUpdatedObject(aclKey)
+        updateIndexedObject(aclKey)
         return success
     }
 
     fun deleteObjectMetadata(aclKey: AclKey) {
         objectMetadata.delete(aclKey)
-        indexDeletedObject(aclKey)
+        deleteIndexedObject(aclKey)
     }
 
     fun deleteObjectMetadataForRootObject(id: UUID) {
@@ -155,45 +154,52 @@ class DatasetService(
 
     fun filterDatasetIdsByOrganizations(datasetIds: Collection<UUID>, organizationIds: Collection<UUID>): Set<UUID> {
         val entitySetIds = entitySets.keySet(
-                Predicates.and(
-                        Predicates.`in`<UUID, EntitySet>(EntitySetMapstore.ORGANIZATION_INDEX, *organizationIds.toTypedArray()),
-                        Predicates.`in`<UUID, EntitySet>(EntitySetMapstore.ID_INDEX, *datasetIds.toTypedArray())
+            Predicates.and(
+                Predicates.`in`<UUID, EntitySet>(
+                    EntitySetMapstore.ORGANIZATION_INDEX,
+                    *organizationIds.toTypedArray()
+                ),
+                Predicates.`in`<UUID, EntitySet>(
+                    EntitySetMapstore.ID_INDEX,
+                    *datasetIds.toTypedArray()
                 )
+            )
         )
 
         val tableIds = externalTables.keySet(
-                Predicates.and(
-                        Predicates.`in`<UUID, ExternalTable>(ExternalTablesMapstore.ORGANIZATION_ID_INDEX, *organizationIds.toTypedArray()),
-                        Predicates.`in`<UUID, ExternalTable>(ExternalTablesMapstore.ID_INDEX, *datasetIds.toTypedArray())
+            Predicates.and(
+                Predicates.`in`<UUID, ExternalTable>(
+                    ExternalTablesMapstore.ORGANIZATION_ID_INDEX,
+                    *organizationIds.toTypedArray()
+                ),
+                Predicates.`in`<UUID, ExternalTable>(
+                    ExternalTablesMapstore.ID_INDEX,
+                    *datasetIds.toTypedArray()
                 )
+            )
         )
 
         return entitySetIds + tableIds
-    }
-
-    fun signalDatasetCreated(datasetId: UUID) {
-        eventBus.post(DatasetCreatedEvent(datasetId))
     }
 
     fun getExternalTables(): Iterable<ExternalTable> {
         return externalTables.values
     }
 
-    private fun indexUpdatedObject(aclKey: AclKey) {
-        val rootObjectId = aclKey.first()
 
-        if (aclKey.size == 1) {
-            eventBus.post(DatasetCreatedEvent(rootObjectId))
-        } else {
-            eventBus.post(DatasetColumnsUpdatedEvent(rootObjectId))
-        }
-    }
-
-    private fun indexDeletedObject(aclKey: AclKey) {
+    private fun deleteIndexedObject(aclKey: AclKey) {
         if (aclKey.size != 1) {
             return
         }
+        searchService.deleteIndexedDataSet(aclKey.first())
+    }
 
-        eventBus.post(DatasetDeletedEvent(aclKey.first()))
+    private fun updateIndexedObject(aclKey: AclKey) {
+        val rootObjectId = aclKey.first()
+        if (aclKey.size == 1) {
+            searchService.updateIndexedDataSet(rootObjectId)
+        } else {
+            searchService.updateIndexedDataSetColumns(rootObjectId)
+        }
     }
 }
