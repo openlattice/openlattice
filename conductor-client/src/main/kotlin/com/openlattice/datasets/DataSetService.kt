c9542ba99deabc61cdc8c177242569c2b1b41f34
Hristo Oskov <hristo@openlattice.com>
2021-06-28 17:32:09 -0700

    rename to "DataSetService"

--- conductor-client/src/main/kotlin/com/openlattice/datasets/DataSetService.kt
+++ conductor-client/src/main/kotlin/com/openlattice/datasets/DataSetService.kt
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class DatasetService(
+class DataSetService(
     hazelcast: HazelcastInstance,
     val searchService: SearchService
 ) {
