70ee6143e65cd53b3f91a45fc0571db4b8ba6f8a
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-09-27 22:47:23 -0700

    logging, fix fetching neighbors

--- conductor-client/src/main/kotlin/com/openlattice/data/jobs/EntitiesAndNeighborsDeletionJob.kt
+++ conductor-client/src/main/kotlin/com/openlattice/data/jobs/EntitiesAndNeighborsDeletionJob.kt
@@ -1,15 +1,12 @@
 package com.openlattice.data.jobs
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.geekbeast.rhizome.jobs.AbstractDistributedJob
-import com.geekbeast.rhizome.jobs.HazelcastJobService
 import com.geekbeast.rhizome.jobs.JobStatus
-import com.geekbeast.util.log
 import com.openlattice.graph.PagedNeighborRequest
-import com.openlattice.graph.core.GraphService
 import com.openlattice.ioc.providers.LateInitAware
 import com.openlattice.ioc.providers.LateInitProvider
 import java.util.*
-import kotlin.streams.toList
 
 /**
  * @author alfoncenzioka &lt;alfonce@openlattice.com&gt;
@@ -17,6 +14,23 @@ import kotlin.streams.toList
 class EntitiesAndNeighborsDeletionJob(
         state: EntitiesAndNeighborsDeletionJobState,
 ) : AbstractDistributedJob<Long, EntitiesAndNeighborsDeletionJobState>(state), LateInitAware {
+
+    @JsonCreator
+    constructor(
+            id: UUID?,
+            taskId: Long?,
+            status: JobStatus,
+            progress: Byte,
+            hasWorkRemaining: Boolean,
+            result: Long?,
+            state: EntitiesAndNeighborsDeletionJobState
+    ) : this(state) {
+        initialize(id, taskId, status, progress, hasWorkRemaining, result)
+    }
+
+    companion object {
+        private const val STATUS_CHECK_INTERVAL = 5000L // 5 seconds
+    }
 
     override val resumable = true
 
@@ -29,22 +43,24 @@ class EntitiesAndNeighborsDeletionJob(
         publishJobState()
     }
 
-
     override fun processNextBatch() {
         val entitySetId = getNextEntitySetId()
         if (entitySetId == null) {
-            logger.info("no more delete jobs to submit for processing")
+            logger.info("No more delete jobs to submit for processing")
             if (allDeleteJobsFinished()) {
                 hasWorkRemaining = false
                 status = JobStatus.FINISHED
                 updateJobStatus()
                 publishJobState()
+                logger.info("finished deleting entities and neighbors of entity set ${state.entitySetId}")
             }
+            Thread.sleep(STATUS_CHECK_INTERVAL)
             return
         }
 
-        // safety measure to avoid deleting all entities from current entity set
-        if (state.entitySetIdEntityKeyIds.getValue(entitySetId).isEmpty()) {
+        // safety check to avoid deleting all entities from current entity set
+        val entityKeyIds = state.entitySetIdEntityKeyIds.getValue(entitySetId)
+        if (entityKeyIds.isEmpty()) {
             logger.info("No entities to delete for entity set $entitySetId")
             return
         }
@@ -55,11 +71,11 @@ class EntitiesAndNeighborsDeletionJob(
                 entitySetId,
                 state.deleteType,
                 state.partitions.getValue(entitySetId),
-                state.entitySetIdEntityKeyIds[entitySetId]?.toMutableSet()
+                entityKeyIds.toMutableSet()
         )))
         state.dataDeletionJobIds.add(jobId)
 
-        logger.info("submitted delete job $jobId for entitySet: $entitySetId")
+        logger.info("submitted delete job $jobId for ${entityKeyIds.size} entities in entity set $entitySetId")
         cleanUpBatch(entitySetId)
         publishJobState()
     }
@@ -77,6 +93,7 @@ class EntitiesAndNeighborsDeletionJob(
 
     private fun getNextEntitySetId(): UUID? {
         val entitySetIds = state.entitySetIdEntityKeyIds.keys
+
         return if (entitySetIds.isEmpty()) null else entitySetIds.first()
     }
 
@@ -84,12 +101,21 @@ class EntitiesAndNeighborsDeletionJob(
     private fun getNeighborEntityKeyIds(): Map<UUID, Set<UUID>> {
 
         val graphService = lateInitProvider.dataGraphService.getGraphService()
+        val entityKeyIds = state.filter.entityKeyIds
 
-        val edges =  graphService.getEdgesAndNeighborsForVertices(setOf(state.entitySetId), PagedNeighborRequest(state.filter))
-        return  edges
-                .toList()
-                .groupBy { it.edge.entitySetId }
-                .mapValues { it -> it.value.mapNotNull { it.edge.entityKeyId }.toSet() }
+        val neighbors = mutableMapOf<UUID, MutableSet<UUID>>()
+
+        graphService.getEdgesAndNeighborsForVertices(setOf(state.entitySetId), PagedNeighborRequest(state.filter)).forEach {
+            val isSrc = entityKeyIds.contains(it.src.entityKeyId)
+
+            val neighborEntityDataKey = if (isSrc) it.dst else it.src
+            val entitySetId = neighborEntityDataKey.entitySetId
+            val entityKeyId = neighborEntityDataKey.entityKeyId
+
+            neighbors.computeIfAbsent(entitySetId) { mutableSetOf() }.add(entityKeyId)
+        }
+
+        return neighbors
     }
 
     override fun setLateInitProvider(lateInitProvider: LateInitProvider) {
