023295aaca4f1acd35ab22badf966be504d2fecc
Matthew Tamayo-Rios <matthew@openlattice.com>
2021-11-03 21:34:07 -1000

    Remove unneeded top utilizers code + fix compile

--- api/src/main/java/com/openlattice/analysis/AnalysisApi.java
+++ api/src/main/java/com/openlattice/analysis/AnalysisApi.java
@@ -18,14 +18,10 @@
 
 package com.openlattice.analysis;
 
-import com.openlattice.analysis.requests.AggregationResult;
 import com.openlattice.analysis.requests.NeighborType;
-import com.openlattice.analysis.requests.RankingAggregation;
 import java.util.UUID;
-
-import com.openlattice.data.requests.FileType;
-
-import retrofit2.http.*;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AnalysisApi {
 
@@ -42,21 +38,6 @@ public interface AnalysisApi {
     String ENTITY_SET_ID_PATH = "/{" + ENTITY_SET_ID + "}";
     String NUM_RESULTS_PATH   = "/{" + NUM_RESULTS + "}";
 
-    /**
-     * Returns the top rows in the entity set, ordered by the sum of the number of items in each of the property types
-     * provided Entity ids and count are included for each row.
-     * 
-     * @param entitySetId The id of the entity set to sort and return results for
-     * @param numResults The number of results to return
-     * @param rankingAggregation
-     * @return
-     */
-    @POST( BASE + ENTITY_SET_ID_PATH + NUM_RESULTS_PATH )
-    AggregationResult getTopUtilizers(
-            @Path( ENTITY_SET_ID ) UUID entitySetId,
-            @Path( NUM_RESULTS ) int numResults,
-            @Body RankingAggregation rankingAggregation,
-            @Query( FILE_TYPE ) FileType fileType );
 
     @GET( BASE + ENTITY_SET_ID_PATH + TYPES_PATH )
     Iterable<NeighborType> getNeighborTypes( @Path( ENTITY_SET_ID ) UUID entitySetId );
